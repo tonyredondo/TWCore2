9b21024d25ea6fa548b46da3feee7dc3d8688f42
daniel.redondo <daniel.redondo@agsw.es>
2017-08-01 10:45:45 +0200

    test

---
+++ test/Tests/CacheTest.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using TWCore.Cache.Storages;
+using TWCore.Cache.Storages.IO;
+using TWCore.Serialization;
+using TWCore.Services;
+
+namespace TWCore.Cache.Test
+{
+    public class CacheTest : ContainerParameterService
+    {
+        public CacheTest() : base("cachetest", "Cache Test") { }
+
+        protected override void OnHandler(ParameterHandlerInfo info)
+        {
+            LRU2QStorageTest();
+            Core.Log.InfoBasic("Press ENTER to continue.");
+            Console.ReadLine();
+            FileStorageTest();
+            Core.Log.InfoBasic("Press ENTER to continue.");
+            Console.ReadLine();
+            CombinedTest();
+            Core.Log.InfoBasic("Press ENTER to exit.");
+            Console.ReadLine();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static void FileStorageTest()
+        {
+            Core.Log.Warning("FileStorage Test");
+            using (var fSto = new FileStorage("./data"))
+            {
+                fSto.Init();
+                Core.Log.InfoBasic("Press ENTER to start the test.");
+                Console.ReadLine();
+                ApplyTest(fSto);
+            }
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static void LRU2QStorageTest()
+        {
+            Core.Log.Warning("LRU2QStorage Test");
+            using (var sto = new LRU2QStorage(20000))
+            {
+                sto.Init();
+                Core.Log.InfoBasic("Press ENTER to start the test.");
+                Console.ReadLine();
+                ApplyTest(sto);
+            }
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static void CombinedTest()
+        {
+            Core.Log.Warning("Combined Test");
+            using (var sto = new LRU2QStorage(20000))
+            {
+                sto.Init();
+                using (var fSto = new FileStorage("./data2"))
+                {
+                    fSto.Init();
+                    var stoManager = new StorageManager();
+                    stoManager.Push(fSto);
+                    stoManager.Push(sto);
+                    Core.Log.InfoBasic("Press ENTER to start the test.");
+                    Console.ReadLine();
+                    ApplyTest(stoManager);
+                }
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        static void ApplyTest(IStorage fSto)
+        {
+            var sobj = new SerializedObject("Esto es un ejemplo del hola mundo");
+
+            using (var w = Watch.Create("Cache GET"))
+            {
+                Parallel.For(0, 25000, i =>
+                {
+                    var value = fSto.Get("Tony - " + i);
+                });
+                Core.Log.InfoBasic("Time per item: {0}ms", w.GlobalElapsedMilliseconds / 10000);
+            }
+            using (var w = Watch.Create("Cache SET"))
+            {
+                Parallel.For(0, 25000, i =>
+                {
+                    fSto.Set("Tony - " + i, sobj);
+                });
+                Core.Log.InfoBasic("Time per item: {0}ms", w.GlobalElapsedMilliseconds / 10000);
+            }
+            using (var w = Watch.Create("Cache GET"))
+            {
+                Parallel.For(0, 25000, i =>
+                {
+                    var value = fSto.Get("Tony - " + i);
+                });
+                Core.Log.InfoBasic("Time per item: {0}ms", w.GlobalElapsedMilliseconds / 10000);
+            }
+        }
+    }
+}

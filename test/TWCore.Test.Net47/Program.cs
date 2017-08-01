fb1923aca6fe68e2a8ac08d94e85dce6edce536b
daniel.redondo <daniel.redondo@agsw.es>
2017-08-01 11:38:56 +0200

    .

---
+++ test/TWCore.Test.Net47/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TWCore;
+using TWCore.Diagnostics.Status.Transports;
+
+namespace TWCore.Test.Net47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Core.DebugMode = true;
+            Core.RunOnInit(() => {
+                Core.Status.Transports.Add(new HttpStatusTransport(8089));
+            });
+            Core.StartContainer(args);
+        }
+    }
+}

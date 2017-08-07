b2ea511719006036c0aebe4ec81d918b5a2924d6
daniel.redondo <daniel.redondo@agsw.es>
2017-08-07 16:52:21 +0200

    New: MySql provider.

---
+++ src/TWCore.Data.MySql/MySqlDataAccess.cs
@@ -0,0 +1,46 @@
+﻿/*
+Copyright 2015-2017 Daniel Adrian Redondo Suarez
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
+using MySql.Data.MySqlClient;
+using System.Data.Common;
+
+namespace TWCore.Data.MySql
+{
+    public class MySqlDataAccess : DataAccessBase
+    {
+        /// <summary>
+        /// Gets the database connection object
+        /// </summary>
+        /// <returns>A DbConnection object</returns>
+        protected override DbConnection GetConnection() => new MySqlConnection();
+        /// <summary>
+        /// Gets the database command object
+        /// </summary>
+        /// <returns>A DbCommand object</returns>
+        protected override DbCommand GetCommand() => new MySqlCommand();
+        /// <summary>
+        /// MySql Server Data access
+        /// </summary>
+        /// <param name="connectionString">Connection string</param>
+        /// <param name="accessType">Data access type</param>
+        public MySqlDataAccess(string connectionString, DataAccessType accessType = DataAccessType.Query)
+        {
+            ConnectionString = connectionString;
+            AccessType = accessType;
+            EntityValueConverter = new MySqlEntityValueConverter(this);
+        }
+    }
+}
---
+++ src/TWCore.Data.MySql/MySqlEntityValueConverter.cs
@@ -0,0 +1,86 @@
+﻿/*
+Copyright 2015-2017 Daniel Adrian Redondo Suarez
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
+using MySql.Data.Types;
+using System;
+using System.Runtime.CompilerServices;
+
+namespace TWCore.Data.MySql
+{
+    /// <summary>
+    /// Value converter for the entity binder for SqlServer databases
+    /// </summary>
+    public class MySqlEntityValueConverter : IEntityValueConverter
+    {
+        MySqlDataAccess _dataAccess;
+
+        #region .ctor
+        /// <summary>
+        /// Value converter for the entity binder for SqlServer databases
+        /// </summary>
+        /// <param name="dataAccess">SqlServerDataAccess instance</param>
+        public MySqlEntityValueConverter(MySqlDataAccess dataAccess)
+        {
+            _dataAccess = dataAccess;
+        }
+        #endregion
+
+        /// <summary>
+        /// Converts a value from the data source to the property type of the entity.
+        /// </summary>
+        /// <param name="value">Value to convert from the data source</param>
+        /// <param name="valueType">Value type from the data source, this is the source type</param>
+        /// <param name="propertyType">Property type of the entity, is the destination type</param>
+        /// <param name="propertyValue">Output parameter as result of the convertion</param>
+        /// <returns>true if the convertion was successful; otherwise, false</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Convert(object value, Type valueType, Type propertyType, out object propertyValue)
+        {
+            propertyValue = propertyType.IsValueType ? Activator.CreateInstance(propertyType) : null;
+            if (value == DBNull.Value)
+                return true;
+            if (value == null && propertyValue == null)
+                return true;
+
+            if (valueType == typeof(MySqlGeometry))
+            {
+                propertyValue = ((MySqlGeometry)value).GetWKT();
+                return true;
+            }
+
+            var dtValue = value as DateTime?;
+            var sdtValue = value as MySqlDateTime?;
+            if (dtValue.HasValue)
+            {
+                propertyValue = dtValue.Value;
+                return true;
+            }
+            if (sdtValue.HasValue)
+            {
+                propertyValue = sdtValue.Value.Value;
+                return true;
+            }
+
+            if (value is IConvertible)
+            {
+                propertyType = propertyType.GetUnderlyingType();
+                propertyValue = System.Convert.ChangeType(value, propertyType);
+                return true;
+            }
+            return false;
+        }
+    }
+}

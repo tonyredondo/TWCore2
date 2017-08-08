46b8812593801769161b76f16114a20bbb8e0f8b
daniel.redondo <daniel.redondo@agsw.es>
2017-08-08 20:03:19 +0200

    RecycleMemoryStream

--- src/TWCore/IO/RecycleMemoryStream.cs
+++ src/TWCore/IO/RecycleMemoryStream.cs
@@ -15,6 +15,8 @@ limitations under the License.
  */
 
 using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.IO;
 using System.Runtime.CompilerServices;
 
@@ -25,8 +27,16 @@ namespace TWCore.IO
     /// </summary>
     public class RecycleMemoryStream : Stream
     {
-        long _length = 0;
-        long _position = 0;
+        static ConcurrentQueue<byte[]> _pool = new ConcurrentQueue<byte[]>();
+        int _maxLength = 255;
+        bool _canWrite = true;
+        int _length = 0;
+        int _maxRow = 0;
+        int _rowIndex = 0;
+        int _position = 0;
+        List<byte[]> _buffer;
+        byte[] _currentBuffer;
+        byte[] _singleByte = new byte[1];
 
         #region Properties
         /// <summary>
@@ -40,20 +50,79 @@ namespace TWCore.IO
         /// <summary>
         /// Gets a value indicating whether the current stream supports writing.
         /// </summary>
-        public override bool CanWrite => true;
+        public override bool CanWrite => _canWrite;
         /// <summary>
         /// Gets the length in bytes of the stream.
         /// </summary>
-        public override long Length => _length;
+        public override long Length => ((_buffer.Count - 1) * _maxLength) + _length;
         /// <summary>
         /// Gets or sets the position within the current stream.
         /// </summary>
         public override long Position
         {
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => _position;
+            get => (_rowIndex * _maxLength) + _position;
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            set => _position = value;
+            set
+            {
+                var val = ((double)value / _maxLength);
+                _rowIndex = (int)val;
+                _position = (int)((val - _rowIndex) * _maxLength);
+                _currentBuffer = _buffer[_rowIndex];
+            }
+        }
+        #endregion
+
+        #region .ctor
+        /// <summary>
+        /// Recycle ByteArray MemoryStream
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public RecycleMemoryStream() : this(null, 0, 0, true) { }
+        /// <summary>
+        /// Recycle ByteArray MemoryStream
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public RecycleMemoryStream(byte[] buffer) : this(buffer, 0, buffer?.Length ?? 0, true) { }
+        /// <summary>
+        /// Recycle ByteArray MemoryStream
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public RecycleMemoryStream(byte[] buffer, bool writable) : this(buffer, 0, buffer?.Length ?? 0, writable) { }
+        /// <summary>
+        /// Recycle ByteArray MemoryStream
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public RecycleMemoryStream(byte[] buffer, int index, int count) : this(buffer, index, count, true) { }
+        /// <summary>
+        /// Recycle ByteArray MemoryStream
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public RecycleMemoryStream(byte[] buffer, int index, int count, bool writable)
+        {
+            _currentBuffer = GetArray();
+            _buffer = new List<byte[]> { _currentBuffer };
+            _maxRow = 0;
+            if (buffer != null)
+                Write(buffer, index, count);
+            _canWrite = writable;
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        ~RecycleMemoryStream()
+        {
+            Dispose(true);
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected override void Dispose(bool disposing)
+        {
+            _currentBuffer = null;
+            if (_buffer != null)
+            {
+                foreach (var array in _buffer)
+                    StoreArray(array);
+                _buffer.Clear();
+            }
+            base.Dispose(disposing);
         }
         #endregion
 
@@ -64,6 +133,71 @@ namespace TWCore.IO
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public override void Flush() { }
         /// <summary>
+        /// Sets the position within the current stream.
+        /// </summary>
+        /// <param name="offset">A byte offset relative to the origin parameter.</param>
+        /// <param name="origin">A value of type System.IO.SeekOrigin indicating the reference point used to obtain the new position.</param>
+        /// <returns>The new position within the current stream.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            var length = ((_buffer.Count - 1) * _maxLength) + _length;
+            var currentPosition = (_rowIndex * _maxLength) + _position;
+            switch (origin)
+            {
+                case SeekOrigin.Begin:
+                    currentPosition = (int)offset;
+                    break;
+                case SeekOrigin.Current:
+                    currentPosition += (int)offset;
+                    break;
+                case SeekOrigin.End:
+                    currentPosition = length + (int)offset;
+                    break;
+            }
+            Position = currentPosition;
+            return currentPosition;
+        }
+        /// <summary>
+        /// Sets the length of the current stream.
+        /// </summary>
+        /// <param name="value">The desired length of the current stream in bytes.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override void SetLength(long value)
+        {
+            var val = ((double)value / _maxLength);
+            var rowIndex = (int)val;
+            var nRows = rowIndex + 1;
+            var nlength = (int)((val - rowIndex) * _maxLength);
+            if (nRows == _buffer.Count)
+            {
+                if (_length > nlength)
+                    Array.Clear(_currentBuffer, nlength - 1, _length - nlength);
+                return;
+            }
+            if (nRows < _buffer.Count)
+            {
+                while (nRows < _buffer.Count)
+                {
+                    StoreArray(_currentBuffer);
+                    var lastIdx = _buffer.Count - 1;
+                    _currentBuffer = _buffer[lastIdx];
+                    _buffer.RemoveAt(lastIdx);
+                    _length = nlength;
+                }
+                Array.Clear(_currentBuffer, _length - 1, _maxLength - _length);
+            }
+            if (nRows > _buffer.Count)
+            {
+                for (var i = _buffer.Count; i < nRows; i++)
+                {
+                    _currentBuffer = GetArray();
+                    _buffer.Add(_currentBuffer);
+                    _maxRow++;
+                }
+            }
+        }
+        /// <summary>
         /// Reads a sequence of bytes from the current stream and advances the position within the stream by the number of bytes read.
         /// </summary>
         /// <param name="buffer">An array of bytes. When this method returns, the buffer contains the specified byte array with the values between offset and (offset + count - 1) replaced by the bytes read from the current source.</param>
@@ -73,27 +207,31 @@ namespace TWCore.IO
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public override int Read(byte[] buffer, int offset, int count)
         {
-            throw new NotImplementedException();
-        }
-        /// <summary>
-        /// Sets the position within the current stream.
-        /// </summary>
-        /// <param name="offset">A byte offset relative to the origin parameter.</param>
-        /// <param name="origin">A value of type System.IO.SeekOrigin indicating the reference point used to obtain the new position.</param>
-        /// <returns>The new position within the current stream.</returns>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public override long Seek(long offset, SeekOrigin origin)
-        {
-            throw new NotImplementedException();
-        }
-        /// <summary>
-        /// Sets the length of the current stream.
-        /// </summary>
-        /// <param name="value">The desired length of the current stream in bytes.</param>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public override void SetLength(long value)
-        {
-            throw new NotImplementedException();
+            int total = 0;
+            while (count > 0)
+            {
+                var clength = (_rowIndex == _maxRow) ? Math.Min(_maxLength, _length) : _maxLength;
+                var remain = clength - _position;
+                if (remain == 0)
+                {
+                    if (_rowIndex < _maxRow)
+                    {
+                        _rowIndex++;
+                        _currentBuffer = _buffer[_rowIndex];
+                        _position = 0;
+                        remain = (_rowIndex == _maxRow) ? Math.Min(_maxLength, _length) : _maxLength;
+                    }
+                    else
+                        return total;
+                }
+                var canRead = Math.Min(count, remain);
+                Buffer.BlockCopy(_currentBuffer, _position, buffer, offset, canRead);
+                count -= canRead;
+                offset += canRead;
+                total += canRead;
+                _position += canRead;
+            }
+            return total;
         }
         /// <summary>
         ///  When overridden in a derived class, writes a sequence of bytes to the current stream and advances the current position within this stream by the number of bytes written.
@@ -104,7 +242,57 @@ namespace TWCore.IO
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public override void Write(byte[] buffer, int offset, int count)
         {
-            throw new NotImplementedException();
+            while (count > 0)
+            {
+                var remain = _maxLength - _position;
+                if (remain == 0)
+                {
+                    _currentBuffer = GetArray();
+                    _buffer.Add(_currentBuffer);
+                    _maxRow++;
+                    _rowIndex++;
+                    _position = 0;
+                    _length = 0;
+                    remain = _maxLength;
+                }
+                var canWrite = Math.Min(count, remain);
+                Buffer.BlockCopy(buffer, offset, _currentBuffer, _position, canWrite);
+                count -= canWrite;
+                offset += canWrite;
+                _position += canWrite;
+                if (_position > _length) _length = _position;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override void WriteByte(byte value)
+        {
+            _singleByte[0] = value;
+            Write(_singleByte, 0, 1);
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override int ReadByte()
+        {
+            if (Read(_singleByte, 0, 1) > 0)
+                return _singleByte[0];
+            return -1;
+        }
+        #endregion
+
+        #region Pool Methods
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        byte[] GetArray()
+        {
+            if (!_pool.TryDequeue(out var res))
+                res = new byte[_maxLength];
+            return res;
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        void StoreArray(byte[] array)
+        {
+            if (array.Length != _maxLength) return;
+            Array.Clear(array, 0, _maxLength);
+            _pool.Enqueue(array);
         }
         #endregion
     }
--- test/TWCore.Test.Net47/Program.cs
+++ test/TWCore.Test.Net47/Program.cs
@@ -19,6 +19,35 @@ namespace TWCore.Test.Net47
             Core.RunOnInit(() =>
             {
                 Core.Status.Transports.Add(new HttpStatusTransport(8089));
+
+                var buffer = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+                for (var m = 0; m < 100000; m++)
+                {
+                    //Console.WriteLine("TEST " + m);
+                    using (var rms = new RecycleMemoryStream())
+                    //using (var rms = new MemoryStream())
+                    {
+                        for (var x = 0; x < 1; x++)
+                        {
+                            for (int i = 0; i < 200; i++)
+                                rms.Write(buffer, 0, buffer.Length);
+                        }
+                        rms.Position = 0;
+                        for (var x = 0; x < 10; x++)
+                        {
+                            //Console.WriteLine();
+                            for (byte i = 0; i < 200; i++)
+                            {
+                                var bt = rms.ReadByte();
+                                //Console.Write(bt + ", ");
+                            }
+                            //Console.WriteLine();
+                        }
+                    }
+                }
+
+                Console.ReadLine();
             });
             Core.StartContainer(args);
         }

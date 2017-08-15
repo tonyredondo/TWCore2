1c052a7c805fa89940895f3102f74bda7257b331
Daniel Redondo <tonyredondo@gmail.com>
2017-08-15 16:36:17 +0200

    NSQ Queue Client.

--- src/TWCore.Messaging.NSQ/NSQueueClient.cs
+++ src/TWCore.Messaging.NSQ/NSQueueClient.cs
@@ -25,6 +25,7 @@ using System.Threading;
 using TWCore.Messaging.Client;
 using TWCore.Messaging.Configuration;
 using TWCore.Messaging.Exceptions;
+using TWCore.Security;
 
 namespace TWCore.Messaging.NSQ
 {
@@ -34,10 +35,12 @@ namespace TWCore.Messaging.NSQ
 	public class NSQueueClient : MQueueClientBase
 	{
 		static readonly ConcurrentDictionary<Guid, NSQueueMessage> ReceivedMessages = new ConcurrentDictionary<Guid, NSQueueMessage>();
+		readonly ConcurrentDictionary<Guid, INsqConsumer> CorrelationIdConsumers = new ConcurrentDictionary<Guid, INsqConsumer>();
 
 		#region Fields
-		List<NsqProducer> _senders;
+		List<(NsqProducer, MQConnection)> _senders;
 		INsqConsumer _receiver;
+		MQConnection _receiverConnection;
 		string _receiverConsumerTag;
 		MQClientQueues _clientQueues;
 		MQClientSenderOptions _senderOptions;
@@ -68,10 +71,66 @@ namespace TWCore.Messaging.NSQ
 		protected override void OnInit()
 		{
 			OnDispose();
-			_senders = new List<NsqProducer>();
+			_senders = new List<(NsqProducer, MQConnection)>();
 			_receiver = null;
 
-			throw new NotImplementedException();
+
+			if (Config != null)
+			{
+				if (Config.ClientQueues != null)
+				{
+					_clientQueues = Config.ClientQueues.FirstOrDefault(c => c.EnvironmentName?.SplitAndTrim(",").Contains(Core.EnvironmentName) == true && c.MachineName?.SplitAndTrim(",").Contains(Core.MachineName) == true)
+									?? Config.ClientQueues.FirstOrDefault(c => c.EnvironmentName?.SplitAndTrim(",").Contains(Core.EnvironmentName) == true)
+									?? Config.ClientQueues.FirstOrDefault(c => c.EnvironmentName.IsNullOrWhitespace());
+				}
+				_senderOptions = Config.RequestOptions?.ClientSenderOptions;
+				_receiverOptions = Config.ResponseOptions?.ClientReceiverOptions;
+				UseSingleResponseQueue = _receiverOptions.Parameters?[ParameterKeys.SingleResponseQueue].ParseTo(false) ?? false;
+
+				if (_clientQueues?.SendQueues?.Any() == true)
+				{
+					foreach (var queue in _clientQueues.SendQueues)
+					{
+						var options = ConsumerOptions.Parse(queue.Route);
+						options.Topic = queue.Name;
+						options.Channel = queue.Name;
+						var nsqProcuder = new NsqProducer(options.NsqEndPoint.Host, options.NsqEndPoint.Port);
+						_senders.Add((nsqProcuder, queue));
+					}
+				}
+				if (_clientQueues?.RecvQueue != null)
+				{
+					_receiverConnection = _clientQueues.RecvQueue;
+					var options = ConsumerOptions.Parse(_receiverConnection.Route);
+					options.Topic = _receiverConnection.Name;
+					options.Channel = _receiverConnection.Name;
+					_receiver = NsqConsumer.Create(options);
+					if (UseSingleResponseQueue)
+					{
+						_receiver.ConnectAndWaitAsync(async message =>
+						{
+							var body = new SubArray<byte>(message.Body);
+							var correlationId = new Guid((byte[])body.Slice(0, 16));
+							var messageBody = (byte[])body.Slice(16);
+							var rMsg = ReceivedMessages.GetOrAdd(correlationId, cId => new NSQueueMessage());
+							rMsg.CorrelationId = correlationId;
+							rMsg.Body = messageBody;
+							rMsg.WaitHandler.Set();
+							await message.FinishAsync();
+						}).WaitAsync();
+						_receiver.SetMaxInFlightAsync(1).WaitAsync();
+					}
+				}
+			}
+
+			Core.Status.Attach(collection =>
+			{
+				if (_senders != null)
+					for (var i = 0; i < _senders.Count; i++)
+						collection.Add(nameof(_senders) + " {0} Path".ApplyFormat(i), _senders[i].Item2.Route);
+				if (_receiver != null)
+					collection.Add(nameof(_receiver) + " Path", _clientQueues.RecvQueue.Route);
+			});
 		}
 		/// <summary>
 		/// On Dispose
@@ -93,16 +152,109 @@ namespace TWCore.Messaging.NSQ
 		#endregion
 
 		#region Send Method
+		/// <summary>
+		/// On Send message data
+		/// </summary>
+		/// <param name="message">Request message instance</param>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		protected override bool OnSend(RequestMessage message)
 		{
-			throw new NotImplementedException();
+			if (_senders?.Any() != true)
+				throw new NullReferenceException("There aren't any senders queues.");
+			if (_senderOptions == null)
+				throw new ArgumentNullException("SenderOptions");
+
+			var recvQueue = _clientQueues.RecvQueue;
+			if (message.Header.ResponseQueue == null)
+			{
+				if (recvQueue != null)
+				{
+					message.Header.ResponseQueue = new MQConnection(recvQueue.Route, recvQueue.Name) { Parameters = recvQueue.Parameters };
+					message.Header.ResponseExpected = true;
+					message.Header.ResponseTimeoutInSeconds = _receiverOptions?.TimeoutInSec ?? -1;
+					if (!UseSingleResponseQueue)
+					{
+						message.Header.ResponseQueue.Name += "_" + message.CorrelationId;
+					}
+				}
+				else
+				{
+					message.Header.ResponseExpected = false;
+					message.Header.ResponseTimeoutInSeconds = -1;
+				}
+			}
+			var data = SenderSerializer.Serialize(message);
+			var body = new byte[data.Count + 16];
+			Buffer.BlockCopy(message.CorrelationId.ToByteArray(), 0, body, 0, 16);
+			data.CopyTo(body, 16);
+
+			foreach ((var sender, var queue) in _senders)
+			{
+				Core.Log.LibVerbose("Sending {0} bytes to the Queue '{1}' with CorrelationId={2}", body.Length, queue.Route + "/" + queue.Name, message.Header.CorrelationId);
+				sender.PublishAsync(queue.Name, body).WaitAsync();
+			}
+			return true;
 		}
 		#endregion
 
 		#region Receive Method
+		/// <summary>
+		/// On Receive message data
+		/// </summary>
+		/// <param name="correlationId">Correlation Id</param>
+		/// <param name="cancellationToken">Cancellation token</param>
+		/// <returns>Response message instance</returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
 		protected override ResponseMessage OnReceive(Guid correlationId, CancellationToken cancellationToken)
 		{
-			throw new NotImplementedException();
+			if (_receiver == null)
+				throw new NullReferenceException("There is not receiver queue.");
+			if (_receiverOptions == null)
+				throw new ArgumentNullException("SenderOptions");
+
+			var timeout = TimeSpan.FromSeconds(_receiverOptions.TimeoutInSec);
+			var sw = Stopwatch.StartNew();
+			var message = ReceivedMessages.GetOrAdd(correlationId, cId => new NSQueueMessage());
+
+			if (!UseSingleResponseQueue)
+			{
+				var _recName = _receiverConnection.Name + "_" + correlationId;
+				var options = ConsumerOptions.Parse(_receiverConnection.Route);
+				options.Topic = _recName;
+				options.Channel = _recName;
+				var rcv = NsqConsumer.Create(options);
+				CorrelationIdConsumers.TryAdd(correlationId, rcv);
+
+				rcv.ConnectAndWaitAsync(async msg =>
+				{
+					var body = new SubArray<byte>(msg.Body);
+					var crId = new Guid((byte[])body.Slice(0, 16));
+					var messageBody = (byte[])body.Slice(16);
+					var rMsg = ReceivedMessages.GetOrAdd(crId, cId => new NSQueueMessage());
+					rMsg.CorrelationId = crId;
+					rMsg.Body = messageBody;
+					rMsg.WaitHandler.Set();
+					await msg.FinishAsync();
+					if (CorrelationIdConsumers.TryRemove(crId, out var _val))
+						_val.Dispose();
+				}).WaitAsync();
+				rcv.SetMaxInFlightAsync(1).WaitAsync();
+			}
+
+			if (message.WaitHandler.Wait(timeout, cancellationToken))
+			{
+				if (message.Body == null)
+					throw new MessageQueueNotFoundException("The Message can't be retrieved, null body on CorrelationId = " + correlationId.ToString());
+
+				Core.Log.LibVerbose("Received {0} bytes from the Queue '{1}' with CorrelationId={2}", message.Body.Length, _clientQueues.RecvQueue.Name, correlationId);
+				var response = ReceiverSerializer.Deserialize<ResponseMessage>(message.Body);
+				Core.Log.LibVerbose("Correlation Message ({0}) received at: {1}ms", correlationId, sw.Elapsed.TotalMilliseconds);
+				sw.Stop();
+				sw = null;
+				return response;
+			}
+			else
+				throw new MessageQueueTimeoutException(timeout, correlationId.ToString());
 		}
 		#endregion
 	}

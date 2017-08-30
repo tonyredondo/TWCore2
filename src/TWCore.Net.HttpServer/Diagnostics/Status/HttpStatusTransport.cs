683ea152be930f92482cb07c07ba8bf556eae4c8
daniel.redondo <daniel.redondo@agsw.es>
2017-08-30 17:54:59 +0200

    fixes.

--- src/TWCore.Net.HttpServer/Diagnostics/Status/HttpStatusTransport.cs
+++ src/TWCore.Net.HttpServer/Diagnostics/Status/HttpStatusTransport.cs
@@ -16,6 +16,7 @@ limitations under the License.
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using TWCore.Net.HttpServer;
 using TWCore.Net.Multicast;
@@ -83,6 +84,24 @@ namespace TWCore.Diagnostics.Status.Transports
                 GC.WaitForPendingFinalizers();
                 ctx.Response.ContentType = SerializerMimeTypes.Json;
                 ctx.Response.Write("true");
+            });
+            httpServer.AddGetHandler("/discovery", ctx =>
+            {
+                var services = DiscoveryService.GetRegisteredServices();
+                var statusServices = services.Where(s => s.Category == DiscoveryService.FRAMEWORK_CATEGORY && s.Name == "STATUS").ToArray();
+                ctx.Response.WriteLine("<html><body><h1>Status services discovered:</h1>");
+                foreach(var g in statusServices.GroupBy(s => new { s.EnvironmentName, s.MachineName }))
+                {
+                    ctx.Response.WriteLine($"<h2>Environment: {g.Key.EnvironmentName}, Machine: {g.Key.MachineName}</h2>");
+                    ctx.Response.WriteLine("<ul>");
+                    foreach (var ss in g)
+                    {
+                        var dct = (Dictionary<string, object>)ss.Data.GetValue();
+                        ctx.Response.WriteLine($"<li><a href='http://{ss.Address.ToString()}:{dct["Port"]}/' target='_blank'>{ss.ApplicationName}</a></li>");
+                    }
+                    ctx.Response.WriteLine("</ul>");
+                }
+                ctx.Response.WriteLine("</body></html>");
             });
             bool connected = false;
             while (true)

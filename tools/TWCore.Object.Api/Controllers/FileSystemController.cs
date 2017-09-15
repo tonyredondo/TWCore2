a0bacfc3047cbd8d583ee683015f72980c183661
daniel.redondo <daniel.redondo@agsw.es>
2017-09-15 18:42:25 +0200

    .

--- tools/TWCore.Object.Api/Controllers/FileSystemController.cs
+++ tools/TWCore.Object.Api/Controllers/FileSystemController.cs
@@ -6,6 +6,7 @@ using System.Runtime.Serialization;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 using Microsoft.AspNetCore.Mvc;
+using TWCore.Serialization;
 using TWCore.Settings;
 
 // ReSharper disable UnusedMember.Global
@@ -17,12 +18,13 @@ namespace TWCore.Object.Api.Controllers
     {
         private static readonly FileSystemSettings Settings = Core.GetSettings<FileSystemSettings>();
         private static readonly PathEntry[] PathEmpty = new PathEntry[0];
+        private static readonly string[] Extensions = SerializerManager.Serializers.Select(s => s.Extensions).SelectMany(i => i).Distinct().ToArray();
 
         // GET api/values
         [HttpGet("api/files/list.{format}")]
         [HttpGet("api/files/list")]
         [FormatFilter]
-        public PathEntry[] Get()
+        public PathEntryCollection Get()
         {
             string virtualPath;
             if (Request.Query.TryGetValue("path", out var folderValues))
@@ -33,17 +35,30 @@ namespace TWCore.Object.Api.Controllers
             var path = Path.Combine(Settings.RootPath, virtualPath);
             path = Path.GetFullPath(path);
             if (!Directory.Exists(path))
-                return PathEmpty;
+                return new PathEntryCollection { Current = virtualPath, Entries =  PathEmpty };
             if (!path.StartsWith(Settings.RootPath))
-                return PathEmpty;
+                return new PathEntryCollection { Current = virtualPath, Entries = PathEmpty };
 
             var rPath = Path.GetFullPath(Settings.RootPath);
 
-            return Directory.EnumerateDirectories(path)
-                .Select(d => new PathEntry { Name  = d.Replace(rPath, string.Empty, StringComparison.OrdinalIgnoreCase), Type =  PathEntryType.Directory })
-                .Concat(Directory.EnumerateFiles(path).Select(d => new PathEntry { Name = d.Replace(rPath, string.Empty, StringComparison.OrdinalIgnoreCase), Type = PathEntryType.File }))
+            var pathEntries = Directory.EnumerateDirectories(path)
+                .Select(d => new PathEntry
+                {
+                    Name  = Path.GetFullPath(d).Replace(rPath, string.Empty, StringComparison.OrdinalIgnoreCase),
+                    Type =  PathEntryType.Directory
+                })
+                .Concat(Directory.EnumerateFiles(path)
+                .Where(file => Extensions.Contains(Path.GetExtension(file), StringComparer.OrdinalIgnoreCase))
+                .Select(d => new PathEntry
+                {
+                    Name = Path.GetFullPath(d).Replace(rPath, string.Empty, StringComparison.OrdinalIgnoreCase),
+                    Type = PathEntryType.File
+                }))
                 .ToArray();
+            return new PathEntryCollection { Current = virtualPath, Entries = pathEntries };
         }
+
+
 
         // GET api/values/5
         [HttpGet("api/files/list/{virtualPath}.{format}")]
@@ -90,6 +105,14 @@ namespace TWCore.Object.Api.Controllers
             public string Name { get; set; }
             [XmlAttribute, DataMember]
             public PathEntryType Type { get; set; }
+        }
+        [DataContract]
+        public class PathEntryCollection
+        {
+            [XmlAttribute, DataMember]
+            public string Current { get; set; }
+            [XmlElement("Entry"), DataMember]
+            public PathEntry[] Entries { get; set; }
         }
         #endregion
     }
--- tools/TWCore.Object.Api/Startup.cs
+++ tools/TWCore.Object.Api/Startup.cs
@@ -27,16 +27,27 @@ namespace TWCore.Object.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.SetDefaultTWCoreValues();
+
+            // Adds a default in-memory implementation of IDistributedCache.
+            services.AddDistributedMemoryCache();
+
+            services.AddSession(options =>
+            {
+                // Set a short timeout for easy testing.
+                options.IdleTimeout = TimeSpan.FromMinutes(60);
+                options.Cookie.Name = ".TWCore.Object.Api.Session";
+                options.Cookie.HttpOnly = true;
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseSession();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
             app.UseMvc();
         }
     }

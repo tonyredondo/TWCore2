080e70a23eb1294b27386693f61960273e7b2c3f
daniel.redondo <daniel.redondo@agsw.es>
2017-09-18 11:57:44 +0200

    improv.

--- tools/TWCore.Object.Api/Controllers/FileSystemController.cs
+++ tools/TWCore.Object.Api/Controllers/FileSystemController.cs
@@ -56,6 +56,90 @@ namespace TWCore.Object.Api.Controllers
             return new PathEntryCollection { Current = virtualPath, Entries = pathEntries };
         }
 
+        [HttpGet("api/files/load.{format}")]
+        [HttpGet("api/files/load")]
+        [FormatFilter]
+        public async Task<bool> LoadFile()
+        {
+            object obj;
+            SessionData sessionData;
+            Type fileType = null;
+
+            if (Request.Query.TryGetValue("type", out var type))
+                fileType = Core.GetType(type);
+            if (!Request.Query.TryGetValue("path", out var path))
+                return false;
+            if (string.IsNullOrWhiteSpace(path))
+                return false;
+            path = Path.GetFullPath(path);
+            if (!System.IO.File.Exists(path))
+                return false;
+
+            var serializer = SerializerManager.GetByFileName(path);
+            if (serializer != null && (serializer.SerializerType == SerializerType.Binary || fileType != null))
+            {
+                Core.Log.InfoBasic("Serializer {0} found, deserializing...", serializer.ToString());
+                obj = serializer.DeserializeFromFile(fileType, path);
+                sessionData = HttpContext.Session.GetSessionData();
+                sessionData.FilePath = path;
+                sessionData.FileObject = obj;
+                HttpContext.Session.SetSessionData(sessionData);
+                Core.Log.InfoBasic("File {0} was loaded.", path);
+                return true;
+            }
+
+            Core.Log.Warning("The serializer for file {0} wasn't found.", path);
+            var extension = Path.GetExtension(path);
+
+            if (string.Equals(extension, ".txt", StringComparison.OrdinalIgnoreCase) || 
+                string.Equals(extension, ".html", StringComparison.OrdinalIgnoreCase) || 
+                string.Equals(extension, ".htm", StringComparison.OrdinalIgnoreCase) ||
+                serializer?.SerializerType == SerializerType.Text)
+            {
+                obj = await System.IO.File.ReadAllTextAsync(path);
+                sessionData = HttpContext.Session.GetSessionData();
+                sessionData.FilePath = path;
+                sessionData.FileObject = obj;
+                HttpContext.Session.SetSessionData(sessionData);
+                Core.Log.InfoBasic("File {0} was loaded as text data.", path);
+                return true;
+            }
+
+            obj = await System.IO.File.ReadAllBytesAsync(path);
+            sessionData = HttpContext.Session.GetSessionData();
+            sessionData.FilePath = path;
+            sessionData.FileObject = obj;
+            HttpContext.Session.SetSessionData(sessionData);
+            Core.Log.InfoBasic("File {0} was loaded as bytes data.", path);
+            return true;
+        }
+
+        [HttpGet("api/files/unload.{format}")]
+        [HttpGet("api/files/unload")]
+        [FormatFilter]
+        public bool UnloadFile()
+        {
+            var sessionData = HttpContext.Session.GetSessionData();
+            if (sessionData.FilePath != null || sessionData.FileObject != null)
+            {
+                var oldFile = sessionData.FilePath;
+                sessionData.FilePath = null;
+                sessionData.FileObject = null;
+                HttpContext.Session.SetSessionData(sessionData);
+                Core.Log.InfoBasic("File {0} was unloaded.", oldFile);
+            }
+            return true;
+        }
+
+        [HttpGet("api/files/status.{format}")]
+        [HttpGet("api/files/status")]
+        [FormatFilter]
+        public string LoadStatus()
+        {
+            var sessionData = HttpContext.Session.GetSessionData();
+            return sessionData.FilePath;
+        }
+
         private static PathEntryCollection GetRootEntryCollection()
         {
             var entries = Settings.RootPaths.Select(p => new PathEntry
---
+++ tools/TWCore.Object.Api/SessionDataExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using TWCore.Serialization;
+
+namespace TWCore.Object.Api
+{
+    public static class SessionDataExtensions
+    {
+        public static SessionData GetSessionData(this ISession session)
+        {
+            return session.TryGetValue("$SESSIONDATA$", out var dataBytes)
+                ? SerializerManager.DefaultBinarySerializer.Deserialize<SessionData>(dataBytes)
+                : new SessionData();
+        }
+        public static void SetSessionData(this ISession session, SessionData data)
+        {
+            var dataBytes = SerializerManager.DefaultBinarySerializer.Serialize(data);
+            session.Set("$SESSIONDATA$", (byte[])dataBytes);
+        }
+    }
+}

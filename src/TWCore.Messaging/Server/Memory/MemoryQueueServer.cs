ecb222b762f1b44b5663f866e1b208a8a25f5f87
Daniel Redondo <tonyredondo@gmail.com>
2017-11-27 22:11:23 +0100

    clone value.

--- src/TWCore.Messaging/Server/Memory/MemoryQueueServer.cs
+++ src/TWCore.Messaging/Server/Memory/MemoryQueueServer.cs
@@ -27,171 +27,179 @@ using TWCore.Threading;
 namespace TWCore.Messaging
 {
 
-    /// <inheritdoc />
-    /// <summary>
-    /// Memory Queue Server Implementation
-    /// </summary>
-    public class MemoryQueueServer : MQueueServerBase
-    {
-        /// <inheritdoc />
-        /// <summary>
-        /// On Create all server listeners
-        /// </summary>
-        /// <param name="connection">Queue server listener</param>
-        /// <param name="responseServer">true if the server is going to act as a response server</param>
-        /// <returns>IMQueueServerListener</returns>
-        protected override IMQueueServerListener OnCreateQueueServerListener(MQConnection connection, bool responseServer = false)
-            => new MemoryQueueServerListener(connection, this, responseServer);
+	/// <inheritdoc />
+	/// <summary>
+	/// Memory Queue Server Implementation
+	/// </summary>
+	public class MemoryQueueServer : MQueueServerBase
+	{
+		/// <inheritdoc />
+		/// <summary>
+		/// On Create all server listeners
+		/// </summary>
+		/// <param name="connection">Queue server listener</param>
+		/// <param name="responseServer">true if the server is going to act as a response server</param>
+		/// <returns>IMQueueServerListener</returns>
+		protected override IMQueueServerListener OnCreateQueueServerListener(MQConnection connection, bool responseServer = false)
+			=> new MemoryQueueServerListener(connection, this, responseServer);
 
-        /// <inheritdoc />
-        /// <summary>
-        /// On Send message data
-        /// </summary>
-        /// <param name="message">Response message instance</param>
-        /// <param name="e">Event Args</param>
-        protected override int OnSend(ResponseMessage message, RequestReceivedEventArgs e)
-        {
-            if (e.ResponseQueues?.Any() != true)
-                return -1;
-            var response = true;
-            foreach (var queue in e.ResponseQueues)
-            {
-                try
-                {
-                    var memQueue = MemoryQueueManager.GetQueue(queue.Route, queue.Name);
-                    Core.Log.LibVerbose("Enqueue message to the memory '{0}' with CorrelationId={1}", queue.Route + "/" + queue.Name, message.CorrelationId);
-                    memQueue.Enqueue(message.CorrelationId, message);
-                }
-                catch (Exception ex)
-                {
-                    response = false;
-                    Core.Log.Write(ex);
-                }
-            }
-            return response ? 1 : -1;
-        }
+		/// <inheritdoc />
+		/// <summary>
+		/// On Send message data
+		/// </summary>
+		/// <param name="message">Response message instance</param>
+		/// <param name="e">Event Args</param>
+		protected override int OnSend(ResponseMessage message, RequestReceivedEventArgs e)
+		{
+			if (e.ResponseQueues?.Any() != true)
+				return -1;
+			var response = true;
+			foreach (var queue in e.ResponseQueues)
+			{
+				try
+				{
+					var memQueue = MemoryQueueManager.GetQueue(queue.Route, queue.Name);
+					Core.Log.LibVerbose("Enqueue message to the memory '{0}' with CorrelationId={1}", queue.Route + "/" + queue.Name, message.CorrelationId);
+					memQueue.Enqueue(message.CorrelationId, message);
+				}
+				catch (Exception ex)
+				{
+					response = false;
+					Core.Log.Write(ex);
+				}
+			}
+			return response ? 1 : -1;
+		}
 
-        /// <inheritdoc />
-        /// <summary>
-        /// On Dispose
-        /// </summary>
-        protected override void OnDispose()
-        {
-        }
-
-
-        #region Nested Classes
-        private class MemoryQueueServerListener : MQueueServerListenerBase
-        {
-            private readonly Type _messageType;
-            private readonly string _name;
-            private MemoryQueue _receiver;
-            private CancellationToken _token;
-
-            #region .ctor
-
-            /// <inheritdoc />
-            /// <summary>
-            /// Memory server listener implementation
-            /// </summary>
-            /// <param name="connection">Queue server listener</param>
-            /// <param name="server">Message queue server instance</param>
-            /// <param name="responseServer">true if the server is going to act as a response server</param>
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            public MemoryQueueServerListener(MQConnection connection, IMQueueServer server, bool responseServer) : base(
-                connection, server, responseServer)
-            {
-                _messageType = responseServer ? typeof(ResponseMessage) : typeof(RequestMessage);
-                _name = server.Name;
-                Core.Status.Attach(collection =>
-                {
-                    collection.Add("Message Type", _messageType);
-                });
-            }
-
-            #endregion
-
-            /// <inheritdoc />
-            /// <summary>
-            /// Start the queue listener for request messages
-            /// </summary>
-            /// <param name="token">Cancellation token</param>
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            protected override async Task OnListenerTaskStartAsync(CancellationToken token)
-            {
-                _token = token;
-                _receiver = MemoryQueueManager.GetQueue(Connection.Route, Connection.Name);
-
-                await Task.Factory.StartNew(() =>
-                {
-                    while (!_token.IsCancellationRequested)
-                    {
-                        var message = _receiver.Dequeue(_token);
-                        if (_token.IsCancellationRequested) break;
-                        EnqueueMessageToProcess(ProcessingTask, message);
-                    }
-                }, token, TaskCreationOptions.LongRunning, TaskScheduler.Default).ConfigureAwait(false);
-
-                WorkerEvent.Wait(TimeSpan.FromSeconds(Config.RequestOptions.ServerReceiverOptions.ProcessingWaitOnFinalizeInSec));
-            }
-
-            /// <inheritdoc />
-            /// <summary>
-            /// On Dispose
-            /// </summary>
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            protected override void OnDispose()
-            {
-            }
+		/// <inheritdoc />
+		/// <summary>
+		/// On Dispose
+		/// </summary>
+		protected override void OnDispose()
+		{
+		}
 
 
-            private void ProcessingTask(MemoryQueue.Message message)
-            {
-                try
-                {
-                    Counters.IncrementProcessingThreads();
-                    Core.Log.LibVerbose("Received message from the memory queue '{0}/{1}'", Connection.Route, Connection.Name);
-                    var messageBody = message.Value;
-                    switch (messageBody)
-                    {
-                        case RequestMessage request when request.Header != null:
-                            {
-                                request.Header.ApplicationReceivedTime = Core.Now;
-                                Counters.IncrementReceivingTime(request.Header.TotalTime);
-                                if (request.Header.ClientName != Config.Name)
-                                    Core.Log.Warning("The Message Client Name '{0}' is different from the Server Name '{1}'", request.Header.ClientName, Config.Name);
-                                var evArgs =
-                                    new RequestReceivedEventArgs(_name, Connection, request, 1);
-                                if (request.Header.ResponseQueue != null)
-                                    evArgs.ResponseQueues.Add(request.Header.ResponseQueue);
-                                OnRequestReceived(evArgs);
-                                break;
-                            }
-                        case ResponseMessage response when response.Header != null:
-                            {
-                                response.Header.Response.ApplicationReceivedTime = Core.Now;
-                                Counters.IncrementReceivingTime(response.Header.Response.TotalTime);
-                                var evArgs =
-                                    new ResponseReceivedEventArgs(_name, response, 1);
-                                OnResponseReceived(evArgs);
-                                break;
-                            }
-                    }
+		#region Nested Classes
+		private class MemoryQueueServerListener : MQueueServerListenerBase
+		{
+			private readonly Type _messageType;
+			private readonly string _name;
+			private MemoryQueue _receiver;
+			private CancellationToken _token;
+			private bool _cloneObject;
 
-                    Counters.IncrementTotalMessagesProccesed();
-                }
-                catch (Exception ex)
-                {
-                    Counters.IncrementTotalExceptions();
-                    Core.Log.Write(ex);
-                }
-                finally
-                {
-                    Counters.DecrementProcessingThreads();
-                }
-            }
+			#region .ctor
 
-        }
-        #endregion
-    }
+			/// <inheritdoc />
+			/// <summary>
+			/// Memory server listener implementation
+			/// </summary>
+			/// <param name="connection">Queue server listener</param>
+			/// <param name="server">Message queue server instance</param>
+			/// <param name="responseServer">true if the server is going to act as a response server</param>
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			public MemoryQueueServerListener(MQConnection connection, IMQueueServer server, bool responseServer) : base(
+				connection, server, responseServer)
+			{
+				_messageType = responseServer ? typeof(ResponseMessage) : typeof(RequestMessage);
+				_name = server.Name;
+				_cloneObject = false;
+				if (connection.Parameters.Contains("CloneObjects"))
+					_cloneObject = connection.Parameters["CloneObjects"].ParseTo(false);
+				Core.Status.Attach(collection =>
+				{
+					collection.Add("Message Type", _messageType);
+				});
+			}
+
+			#endregion
+
+			/// <inheritdoc />
+			/// <summary>
+			/// Start the queue listener for request messages
+			/// </summary>
+			/// <param name="token">Cancellation token</param>
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			protected override async Task OnListenerTaskStartAsync(CancellationToken token)
+			{
+				_token = token;
+				_receiver = MemoryQueueManager.GetQueue(Connection.Route, Connection.Name);
+
+				await Task.Factory.StartNew(() =>
+				{
+					while (!_token.IsCancellationRequested)
+					{
+						var message = _receiver.Dequeue(_token);
+						if (_token.IsCancellationRequested) break;
+						EnqueueMessageToProcess(ProcessingTask, message);
+					}
+				}, token, TaskCreationOptions.LongRunning, TaskScheduler.Default).ConfigureAwait(false);
+
+				WorkerEvent.Wait(TimeSpan.FromSeconds(Config.RequestOptions.ServerReceiverOptions.ProcessingWaitOnFinalizeInSec));
+			}
+
+			/// <inheritdoc />
+			/// <summary>
+			/// On Dispose
+			/// </summary>
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			protected override void OnDispose()
+			{
+			}
+
+
+			private void ProcessingTask(MemoryQueue.Message message)
+			{
+				try
+				{
+					Counters.IncrementProcessingThreads();
+					Core.Log.LibVerbose("Received message from the memory queue '{0}/{1}'", Connection.Route, Connection.Name);
+					var messageBody = message.Value;
+					switch (messageBody)
+					{
+						case RequestMessage request when request.Header != null:
+							{
+								if (_cloneObject)
+									request.Body = request.Body.DeepClone();
+								request.Header.ApplicationReceivedTime = Core.Now;
+								Counters.IncrementReceivingTime(request.Header.TotalTime);
+								if (request.Header.ClientName != Config.Name)
+									Core.Log.Warning("The Message Client Name '{0}' is different from the Server Name '{1}'", request.Header.ClientName, Config.Name);
+								var evArgs =
+									new RequestReceivedEventArgs(_name, Connection, request, 1);
+								if (request.Header.ResponseQueue != null)
+									evArgs.ResponseQueues.Add(request.Header.ResponseQueue);
+								OnRequestReceived(evArgs);
+								break;
+							}
+						case ResponseMessage response when response.Header != null:
+							{
+								if (_cloneObject)
+									response.Body = response.Body.DeepClone();
+								response.Header.Response.ApplicationReceivedTime = Core.Now;
+								Counters.IncrementReceivingTime(response.Header.Response.TotalTime);
+								var evArgs =
+									new ResponseReceivedEventArgs(_name, response, 1);
+								OnResponseReceived(evArgs);
+								break;
+							}
+					}
+
+					Counters.IncrementTotalMessagesProccesed();
+				}
+				catch (Exception ex)
+				{
+					Counters.IncrementTotalExceptions();
+					Core.Log.Write(ex);
+				}
+				finally
+				{
+					Counters.DecrementProcessingThreads();
+				}
+			}
+
+		}
+		#endregion
+	}
 }

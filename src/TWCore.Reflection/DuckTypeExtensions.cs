9e359ab4c7f352bf02689d4ea893177d37a37117
Daniel Redondo <tonyredondo@gmail.com>
2017-11-27 21:52:09 +0100

    New Ducktype

---
+++ src/TWCore.Reflection/DuckTypeExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using TWCore.Reflection;
+
+namespace TWCore
+{
+	/// <summary>
+	/// DuckType extensions
+	/// </summary>
+	public static class DuckTypeExtensions
+	{
+		/// <summary>
+		/// Creates a new proxy object arround the current object using an interface type for duck typing
+		/// </summary>
+		/// <typeparam name="T">Interface type to proxy</typeparam>
+		/// <param name="obj">Real object instance</param>
+		/// <returns>Proxy object instance</returns>
+		public static T ActAs<T>(this object obj) where T : class
+		{
+			if (obj is T && obj is DuckTypeProxy)
+				return obj as T;
+			return DuckTypeProxy.Create<T>(obj);
+		}
+
+		/// <summary>
+		/// Gets the original object from the proxy instance
+		/// </summary>
+		/// <typeparam name="T">Type of real object</typeparam>
+		/// <param name="proxy">Proxy object instance</param>
+		/// <returns>Original object instance</returns>
+		public static T UnwrapProxy<T>(this object proxy) where T : class
+		{
+			if (proxy == null) return null;
+			if (proxy is DuckTypeProxy dtProxy)
+				return dtProxy.RealObject as T;
+			return proxy as T;
+		}
+	}
+}
---
+++ src/TWCore.Reflection/DuckTypeProxy.cs
@@ -0,0 +1,395 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Dynamic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Microsoft.CSharp.RuntimeBinder;
+using TWCore;
+using TWCore.Reflection;
+
+namespace TWCore.Reflection
+{
+	/// <summary>
+	/// DuckType Container for Proxy objects
+	/// </summary>
+	public abstract class DuckTypeProxy
+	{
+		#region Private fields
+		[DebuggerBrowsable(DebuggerBrowsableState.Never)]
+		ConcurrentDictionary<string, MethodAccessorDelegate> _methodsDelegates = null;
+		[DebuggerBrowsable(DebuggerBrowsableState.Never)]
+		ConcurrentDictionary<string, FastPropertyInfo> _propertyInfo = null;
+		[DebuggerBrowsable(DebuggerBrowsableState.Never)]
+		ConcurrentDictionary<(string Method, string Types), CallSite> _dynMethodDelegates = null;
+		[DebuggerBrowsable(DebuggerBrowsableState.Never)]
+		object _realObject;
+		#endregion
+
+		#region Properties
+		/// <summary>
+		/// Get current object
+		/// </summary>
+		public object RealObject => _realObject;
+		#endregion
+
+		#region Private Methods
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		void SetRealObject(object realObject)
+		{
+			if (realObject == null) return;
+			_realObject = realObject;
+			var containerType = realObject.GetType();
+			if (_realObject is DynamicObject)
+			{
+				_dynMethodDelegates = new ConcurrentDictionary<(string, string), CallSite>();
+			}
+			else
+			{
+				_methodsDelegates = new ConcurrentDictionary<string, MethodAccessorDelegate>();
+				var methods = containerType.GetMethods();
+				for (var i = 0; i < methods.Length; i++)
+				{
+					var method = methods[i];
+					var accessor = method.GetMethodAccessor();
+					method.GetParameters();
+					_methodsDelegates.TryAdd(method.Name, accessor);
+				}
+			}
+			_propertyInfo = new ConcurrentDictionary<string, FastPropertyInfo>();
+			var properties = containerType.GetProperties();
+			for (var i = 0; i < properties.Length; i++)
+			{
+				var property = properties[i];
+				var fastProperty = property.GetFastPropertyInfo();
+				_propertyInfo.TryAdd(property.Name, fastProperty);
+			}
+		}
+		#endregion
+
+		#region Protected Methods
+		/// <summary>
+		/// Calls a method on the real object dynamically
+		/// </summary>
+		/// <param name="methodName">Method name</param>
+		/// <param name="args">Method parameters</param>
+		/// <returns>Method return value</returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		protected object CallMethod(string methodName, params object[] args)
+		{
+			args = args ?? new object[0];
+
+			if (_methodsDelegates != null && _methodsDelegates.TryGetValue(methodName, out var mDelegate))
+				return mDelegate(_realObject, args);
+
+			var dContainer = _realObject as DynamicObject;
+			if (dContainer == null)
+				throw new MissingMethodException(_realObject?.GetType().Name, methodName);
+
+			var types = new List<Type>(args.Length);
+			var typesKey = string.Empty;
+			for (var i = 0; i < args.Length; i++)
+			{
+				var arg = args[i];
+				var type = arg?.GetType() ?? typeof(object);
+				types.Add(type);
+				typesKey += type.FullName;
+			}
+
+			var callSite = _dynMethodDelegates.GetOrAdd((methodName, typesKey), _mName =>
+			{
+				var cSiteBinder = Microsoft.CSharp.RuntimeBinder.Binder.InvokeMember(CSharpBinderFlags.None, _mName.Method, types, this.GetType(),
+					(new[] { CSharpArgumentInfo.Create(CSharpArgumentInfoFlags.None, null) }).Concat(args.Select(a => CSharpArgumentInfo.Create(CSharpArgumentInfoFlags.None, null)))
+					);
+				switch (args.Length)
+				{
+					case 0:
+						return CallSite<Func<CallSite, object, object>>.Create(cSiteBinder);
+					case 1:
+						return CallSite<Func<CallSite, object, object, object>>.Create(cSiteBinder);
+					case 2:
+						return CallSite<Func<CallSite, object, object, object, object>>.Create(cSiteBinder);
+					case 3:
+						return CallSite<Func<CallSite, object, object, object, object, object>>.Create(cSiteBinder);
+					case 4:
+						return CallSite<Func<CallSite, object, object, object, object, object, object>>.Create(cSiteBinder);
+					case 5:
+						return CallSite<Func<CallSite, object, object, object, object, object, object, object>>.Create(cSiteBinder);
+					case 6:
+						return CallSite<Func<CallSite, object, object, object, object, object, object, object, object>>.Create(cSiteBinder);
+					case 7:
+						return CallSite<Func<CallSite, object, object, object, object, object, object, object, object, object>>.Create(cSiteBinder);
+					case 8:
+						return CallSite<Func<CallSite, object, object, object, object, object, object, object, object, object, object>>.Create(cSiteBinder);
+					case 9:
+						return CallSite<Func<CallSite, object, object, object, object, object, object, object, object, object, object, object>>.Create(cSiteBinder);
+					case 10:
+						return CallSite<Func<CallSite, object, object, object, object, object, object, object, object, object, object, object, object>>.Create(cSiteBinder);
+				}
+				return null;
+			});
+
+			switch (args.Length)
+			{
+				case 0:
+					return ((CallSite<Func<CallSite, object, object>>)callSite).Target(callSite, dContainer);
+				case 1:
+					return ((CallSite<Func<CallSite, object, object, object>>)callSite).Target(callSite, dContainer, args[0]);
+				case 2:
+					return ((CallSite<Func<CallSite, object, object, object, object>>)callSite).Target(callSite, dContainer, args[0], args[1]);
+				case 3:
+					return ((CallSite<Func<CallSite, object, object, object, object, object>>)callSite).Target(callSite, dContainer, args[0], args[1], args[2]);
+				case 4:
+					return ((CallSite<Func<CallSite, object, object, object, object, object, object>>)callSite).Target(callSite, dContainer, args[0], args[1], args[2], args[3]);
+				case 5:
+					return ((CallSite<Func<CallSite, object, object, object, object, object, object, object>>)callSite).Target(callSite, dContainer, args[0], args[1], args[2], args[3], args[4]);
+				case 6:
+					return ((CallSite<Func<CallSite, object, object, object, object, object, object, object, object>>)callSite).Target(callSite, dContainer, args[0], args[1], args[2], args[3], args[4], args[5]);
+				case 7:
+					return ((CallSite<Func<CallSite, object, object, object, object, object, object, object, object, object>>)callSite).Target(callSite, dContainer, args[0], args[1], args[2], args[3], args[4], args[5], args[6]);
+				case 8:
+					return ((CallSite<Func<CallSite, object, object, object, object, object, object, object, object, object, object>>)callSite).Target(callSite, dContainer, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7]);
+				case 9:
+					return ((CallSite<Func<CallSite, object, object, object, object, object, object, object, object, object, object, object>>)callSite).Target(callSite, dContainer, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8]);
+				case 10:
+					return ((CallSite<Func<CallSite, object, object, object, object, object, object, object, object, object, object, object, object>>)callSite).Target(callSite, dContainer, args[0], args[1], args[2], args[3], args[4], args[5], args[6], args[7], args[8], args[9]);
+			}
+			return null;
+		}
+		/// <summary>
+		/// Get a Property value from the real object dynamically
+		/// </summary>
+		/// <param name="propertyName">Property name</param>
+		/// <returns>Property value</returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		protected object GetProperty(string propertyName)
+		{
+			if (!_propertyInfo.TryGetValue(propertyName, out var fProperty))
+				throw new MissingMemberException(_realObject?.GetType().Name, propertyName);
+			return fProperty.GetValue(_realObject);
+		}
+		/// <summary>
+		/// Set a Property value to the real object dynamically
+		/// </summary>
+		/// <param name="propertyName">Property name</param>
+		/// <param name="value">Property value</param>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		protected void SetProperty(string propertyName, object value)
+		{
+			if (!_propertyInfo.TryGetValue(propertyName, out var fProperty))
+				throw new MissingMemberException(_realObject?.GetType().Name, propertyName);
+			fProperty.SetValue(_realObject, value);
+		}
+		#endregion
+
+		#region Public Static Methods
+		/// <summary>
+		/// Create a new Proxy object using an interface for ducktyping.
+		/// </summary>
+		/// <typeparam name="T">Type of interface</typeparam>
+		/// <param name="realObject">Object to be proxied</param>
+		/// <returns>Proxy instance</returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static T Create<T>(object realObject) => (T)Create(typeof(T), realObject);
+		/// <summary>
+		/// Create a new Proxy object using an interface for ducktyping.
+		/// </summary>
+		/// <param name="interfaceType">Type of interface</param>
+		/// <param name="realObject">Object to be proxied</param>
+		/// <returns>Proxy instance</returns>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public static object Create(Type interfaceType, object realObject)
+		{
+			if (interfaceType == null)
+				throw new ArgumentNullException(nameof(interfaceType), "The Interface type can't be null");
+			if (realObject == null)
+				throw new ArgumentNullException(nameof(realObject), "The original object can't be null");
+			if (realObject is Task)
+				throw new ArgumentNullException(nameof(realObject), "The original object can't be a Task");
+
+			var typeSignature = "ProxyTo" + realObject.GetType().Name;
+			//Create Type
+			var an = new AssemblyName(typeSignature + "Assembly");
+			var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(an, AssemblyBuilderAccess.Run);
+			var moduleBuilder = assemblyBuilder.DefineDynamicModule("MainModule");
+			var typeBuilder = moduleBuilder.DefineType(typeSignature, TypeAttributes.Public | TypeAttributes.Class | TypeAttributes.AutoClass | TypeAttributes.AnsiClass | TypeAttributes.BeforeFieldInit | TypeAttributes.AutoLayout,
+				typeof(DuckTypeProxy), new Type[] { interfaceType });
+			var constructor = typeBuilder.DefineDefaultConstructor(MethodAttributes.Public);
+
+			//Create Members
+			CreateInterfaceMethods(interfaceType, typeBuilder);
+			CreateInterfaceProperties(interfaceType, typeBuilder);
+
+			//Create Type instance
+			var type = typeBuilder.CreateTypeInfo().AsType();
+			var objType = (DuckTypeProxy)Activator.CreateInstance(type);
+			objType.SetRealObject(realObject);
+			return objType;
+		}
+		#endregion
+
+		#region Private Static Methods
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static void CreateInterfaceProperties(Type interfaceType, TypeBuilder typeBuilder)
+		{
+			var getPropertyMethod = typeof(DuckTypeProxy).GetMethod("GetProperty", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic, null, new Type[] { typeof(string) }, null);
+			var setPropertyMethod = typeof(DuckTypeProxy).GetMethod("SetProperty", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic, null, new Type[] { typeof(string), typeof(object) }, null);
+			var debAttrCtor = typeof(DebuggerBrowsableAttribute).GetConstructor(new Type[] { typeof(DebuggerBrowsableState) });
+			var interfaceProperties = interfaceType.GetProperties();
+			for (var i = 0; i < interfaceProperties.Length; i++)
+			{
+				var iProperty = interfaceProperties[i];
+				PropertyBuilder propertyBuilder = typeBuilder.DefineProperty(iProperty.Name, PropertyAttributes.HasDefault, iProperty.PropertyType, null);
+				propertyBuilder.SetCustomAttribute(new CustomAttributeBuilder(debAttrCtor, new object[] { DebuggerBrowsableState.Never }));
+				if (iProperty.CanRead)
+				{
+					MethodBuilder getPropMthdBldr = typeBuilder.DefineMethod("get_" + iProperty.Name, MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.HideBySig, iProperty.PropertyType, Type.EmptyTypes);
+					var gen = getPropMthdBldr.GetILGenerator();
+					var label = gen.DefineLabel();
+					gen.Emit(OpCodes.Nop);
+					gen.Emit(OpCodes.Ldarg_0);
+					gen.Emit(OpCodes.Ldstr, iProperty.Name);
+					gen.Emit(OpCodes.Call, getPropertyMethod);
+					if (iProperty.PropertyType.IsValueType)
+						gen.Emit(OpCodes.Unbox_Any, iProperty.PropertyType);
+					else
+						gen.Emit(OpCodes.Castclass, iProperty.PropertyType);
+					gen.Emit(OpCodes.Stloc_0);
+					gen.Emit(OpCodes.Br_S, label);
+					gen.MarkLabel(label);
+					gen.Emit(OpCodes.Ldloc_0);
+					gen.Emit(OpCodes.Ret);
+					propertyBuilder.SetGetMethod(getPropMthdBldr);
+				}
+				if (iProperty.CanWrite)
+				{
+					MethodBuilder setPropMthdBldr = typeBuilder.DefineMethod("set_" + iProperty.Name, MethodAttributes.Public | MethodAttributes.SpecialName | MethodAttributes.HideBySig, null, new[] { iProperty.PropertyType });
+					var gen = setPropMthdBldr.GetILGenerator();
+					gen.Emit(OpCodes.Nop);
+					gen.Emit(OpCodes.Ldarg_0);
+					gen.Emit(OpCodes.Ldstr, iProperty.Name);
+					gen.Emit(OpCodes.Ldarg_1);
+					if (iProperty.PropertyType.IsValueType)
+						gen.Emit(OpCodes.Box, iProperty.PropertyType);
+					gen.Emit(OpCodes.Call, setPropertyMethod);
+					gen.Emit(OpCodes.Nop);
+					gen.Emit(OpCodes.Ret);
+					propertyBuilder.SetSetMethod(setPropMthdBldr);
+				}
+			}
+		}
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static void CreateInterfaceMethods(Type interfaceType, TypeBuilder typeBuilder)
+		{
+			var callMethod = typeof(DuckTypeProxy).GetMethod("CallMethod", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic, null, new Type[] { typeof(string), typeof(object[]) }, null);
+			var interfaceMethods = interfaceType.GetMethods();
+			for (var i = 0; i < interfaceMethods.Length; i++)
+			{
+				var iMethod = interfaceMethods[i];
+				var parameters = iMethod.GetParameters();
+
+				var paramBuilders = new ParameterBuilder[parameters.Length];
+				var methodBuilder = typeBuilder.DefineMethod(iMethod.Name, MethodAttributes.Public | MethodAttributes.Virtual | MethodAttributes.Final | MethodAttributes.HideBySig | MethodAttributes.NewSlot, iMethod.ReturnType, parameters.Select(p => p.ParameterType).ToArray());
+				for (var j = 0; j < parameters.Length; j++)
+					paramBuilders[j] = methodBuilder.DefineParameter(j, ParameterAttributes.None, parameters[j].Name);
+
+				var gen = methodBuilder.GetILGenerator();
+
+				LocalBuilder loc = null;
+				Label label = new Label();
+				if (iMethod.ReturnType != typeof(void))
+				{
+					loc = gen.DeclareLocal(iMethod.ReturnType);
+					label = gen.DefineLabel();
+				}
+
+				gen.Emit(OpCodes.Nop);
+				gen.Emit(OpCodes.Ldarg_0);
+				gen.Emit(OpCodes.Ldstr, iMethod.Name);
+				if (parameters.Length == 0) gen.Emit(OpCodes.Ldc_I4_0);
+				if (parameters.Length == 1) gen.Emit(OpCodes.Ldc_I4_1);
+				if (parameters.Length == 2) gen.Emit(OpCodes.Ldc_I4_2);
+				if (parameters.Length == 3) gen.Emit(OpCodes.Ldc_I4_3);
+				if (parameters.Length == 4) gen.Emit(OpCodes.Ldc_I4_4);
+				if (parameters.Length == 5) gen.Emit(OpCodes.Ldc_I4_5);
+				if (parameters.Length == 6) gen.Emit(OpCodes.Ldc_I4_6);
+				if (parameters.Length == 7) gen.Emit(OpCodes.Ldc_I4_7);
+				if (parameters.Length == 8) gen.Emit(OpCodes.Ldc_I4_8);
+				if (parameters.Length == 9) gen.Emit(OpCodes.Ldc_I4_S, 9);
+				gen.Emit(OpCodes.Newarr, typeof(object));
+
+				if (parameters.Length > 0)
+				{
+
+					for (var j = 0; j < parameters.Length; j++)
+					{
+						gen.Emit(OpCodes.Dup);
+						switch (j)
+						{
+							case 0:
+								gen.Emit(OpCodes.Ldc_I4_0);
+								gen.Emit(OpCodes.Ldarg_1);
+								break;
+							case 1:
+								gen.Emit(OpCodes.Ldc_I4_1);
+								gen.Emit(OpCodes.Ldarg_2);
+								break;
+							case 2:
+								gen.Emit(OpCodes.Ldc_I4_2);
+								gen.Emit(OpCodes.Ldarg_3);
+								break;
+							case 3:
+								gen.Emit(OpCodes.Ldc_I4_3);
+								gen.Emit(OpCodes.Ldarg_S, j + 1);
+								break;
+							case 4:
+								gen.Emit(OpCodes.Ldc_I4_4);
+								gen.Emit(OpCodes.Ldarg_S, j + 1);
+								break;
+							case 5:
+								gen.Emit(OpCodes.Ldc_I4_5);
+								gen.Emit(OpCodes.Ldarg_S, j + 1);
+								break;
+							case 6:
+								gen.Emit(OpCodes.Ldc_I4_6);
+								gen.Emit(OpCodes.Ldarg_S, j + 1);
+								break;
+							case 7:
+								gen.Emit(OpCodes.Ldc_I4_7);
+								gen.Emit(OpCodes.Ldarg_S, j + 1);
+								break;
+							case 8:
+								gen.Emit(OpCodes.Ldc_I4_8);
+								gen.Emit(OpCodes.Ldarg_S, j + 1);
+								break;
+						}
+						if (iMethod.ReturnType.IsValueType)
+							gen.Emit(OpCodes.Box, parameters[j].ParameterType);
+						gen.Emit(OpCodes.Stelem_Ref);
+					}
+				}
+				gen.Emit(OpCodes.Call, callMethod);
+				if (iMethod.ReturnType != typeof(void))
+				{
+					if (iMethod.ReturnType.IsValueType)
+						gen.Emit(OpCodes.Unbox_Any, iMethod.ReturnType);
+					else
+						gen.Emit(OpCodes.Castclass, iMethod.ReturnType);
+					gen.Emit(OpCodes.Stloc_0);
+					gen.Emit(OpCodes.Br_S, label);
+					gen.MarkLabel(label);
+					gen.Emit(OpCodes.Ldloc_0);
+				}
+				else
+					gen.Emit(OpCodes.Pop);
+
+				gen.Emit(OpCodes.Ret);
+			}
+		}
+		#endregion
+	}
+}

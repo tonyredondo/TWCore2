baef54911f38b7d25c7bfe76c61afe2673da3006
Daniel Redondo <tonyredondo@gmail.com>
2018-05-08 20:21:46 +0200

    changes

---
+++ tools/TWCore.Diagnostics.Api.Models/IMessageDiagnosticHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using TWCore.Diagnostics.Log;
+using TWCore.Diagnostics.Status;
+using TWCore.Diagnostics.Trace.Storages;
+// ReSharper disable UnusedMember.Global
+
+namespace TWCore.Diagnostics.Api.Models
+{
+    /// <summary>
+    /// Message diagnostic handler interface
+    /// </summary>
+    public interface IMessageDiagnosticHandler
+    {
+        /// <summary>
+        /// Process LogItems message
+        /// </summary>
+        /// <param name="message">Message to handle</param>
+        /// <returns>Process task</returns>
+        Task ProcessLogItemsMessage(List<LogItem> message);
+        /// <summary>
+        /// Process TraceItems message
+        /// </summary>
+        /// <param name="message">Message to handle</param>
+        /// <returns>Process task</returns>
+        Task ProcessTraceItemsMessage(List<MessagingTraceItem> message);
+        /// <summary>
+        /// Process Status message
+        /// </summary>
+        /// <param name="message">Message to handle</param>
+        /// <returns>Process task</returns>
+        Task ProcessStatusMessage(StatusItemCollection message);
+    }
+}

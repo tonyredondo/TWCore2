00c91dfdd8d80d990d98eab712e67606ee9a60e1
daniel.redondo <daniel.redondo@agsw.es>
2018-06-11 18:26:40 +0200

    docker file

---
+++ tools/TWCore.Diagnostics.Api/dockerBuild.sh
@@ -0,0 +1,11 @@
+﻿echo "Remove previous build"
+rm -r app
+
+echo "Creating folder structure..."
+mkdir app
+
+echo "Publishing project..."
+dotnet publish -c Release -r linux-x64 -v q -o ./app/
+
+echo "Building docker image"
+docker build -t twcore_diagnostics:2.0.135 .

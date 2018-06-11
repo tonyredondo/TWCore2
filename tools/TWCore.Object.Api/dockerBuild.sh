89e1279960f64790499f452d38a780bf9983d62b
daniel.redondo <daniel.redondo@agsw.es>
2018-06-11 18:58:18 +0200

    object api docker file

---
+++ tools/TWCore.Object.Api/dockerBuild.sh
@@ -0,0 +1,14 @@
+﻿echo "Remove previous build"
+rm -r app
+
+echo "Creating folder structure..."
+mkdir app
+
+echo "Publishing project..."
+dotnet publish -c Release -r linux-x64 -v q -o ./app/
+
+echo "Building docker image"
+docker build -t twcore_objectviewer:2.0.135 .
+
+echo "Remove build artifacts"
+rm -r app

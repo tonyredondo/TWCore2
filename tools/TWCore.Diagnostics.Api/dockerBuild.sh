f403e169dc6bc0694826f6ba278525a4b373f8ec
daniel.redondo <daniel.redondo@agsw.es>
2018-06-19 11:19:23 +0200

    Version update.

--- tools/TWCore.Diagnostics.Api/dockerBuild.sh
+++ tools/TWCore.Diagnostics.Api/dockerBuild.sh
@@ -8,9 +8,9 @@ echo "Publishing project..."
 dotnet publish -c Release -r linux-x64 -v q -o ./app/
 
 echo "Building docker image"
-docker build -t twcore_diagnostics:2.0.140 .
-docker tag twcore_diagnostics:2.0.140 tonyredondo/twcore_diagnostics:2.0.140
-docker push tonyredondo/twcore_diagnostics:2.0.140
+docker build -t twcore_diagnostics:2.1.141 .
+docker tag twcore_diagnostics:2.1.141 tonyredondo/twcore_diagnostics:2.1.141
+docker push tonyredondo/twcore_diagnostics:2.1.141
 
 echo "Remove build artifacts"
 rm -r app
--- tools/TWCore.Object.Api/dockerBuild.sh
+++ tools/TWCore.Object.Api/dockerBuild.sh
@@ -8,9 +8,9 @@ echo "Publishing project..."
 dotnet publish -c Release -r linux-x64 -v q -o ./app/
 
 echo "Building docker image"
-docker build -t twcore_objectviewer:2.0.140 .
-docker tag twcore_objectviewer:2.0.140 tonyredondo/twcore_objectviewer:2.0.140
-docker push tonyredondo/twcore_objectviewer:2.0.140
+docker build -t twcore_objectviewer:2.1.141 .
+docker tag twcore_objectviewer:2.1.141 tonyredondo/twcore_objectviewer:2.1.141
+docker push tonyredondo/twcore_objectviewer:2.1.141
 
 echo "Remove build artifacts"
 rm -r app

73ccb50d953a30085f6eb282c8476c72ae8c94fa
daniel.redondo <daniel.redondo@agsw.es>
2018-06-28 12:52:40 +0200

    version upgrade

--- tools/TWCore.Object.Api/dockerBuild.sh
+++ tools/TWCore.Object.Api/dockerBuild.sh
@@ -8,9 +8,9 @@ echo "Publishing project..."
 dotnet publish -c Release -r linux-x64 -v q -o ./app/
 
 echo "Building docker image"
-docker build -t twcore_objectviewer:2.1.143 .
-docker tag twcore_objectviewer:2.1.143 tonyredondo/twcore_objectviewer:2.1.143
-docker push tonyredondo/twcore_objectviewer:2.1.143
+docker build -t twcore_objectviewer:2.1.144 .
+docker tag twcore_objectviewer:2.1.144 tonyredondo/twcore_objectviewer:2.1.144
+docker push tonyredondo/twcore_objectviewer:2.1.144
 
 echo "Remove build artifacts"
 rm -r app

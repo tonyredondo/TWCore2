0a699da3dde8026364654ef1645c63a123763f80
Daniel Redondo <tonyredondo@gmail.com>
2018-06-30 14:38:54 +0200

    Changes.

---
+++ tools/TWCore.Diagnostics.Api.Models/SearchResults.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Xml.Serialization;
+using TWCore.Diagnostics.Api.Models.Log;
+using TWCore.Diagnostics.Api.Models.Trace;
+
+namespace TWCore.Diagnostics.Api.Models
+{
+    [DataContract]
+    public class SearchResults
+    {
+        [XmlElement, DataMember]
+        public List<NodeLogItem> Logs { get; set; }
+        [XmlElement, DataMember]
+        public List<NodeTraceItem> Traces { get; set; }
+    }
+}
--- tools/TWCore.Diagnostics.Api/MessageHandlers/RavenDb/RavenDbQueryHandler.cs
+++ tools/TWCore.Diagnostics.Api/MessageHandlers/RavenDb/RavenDbQueryHandler.cs
@@ -33,34 +33,27 @@ namespace TWCore.Diagnostics.Api.MessageHandlers.RavenDb
 {
     public class RavenDbQueryHandler : IDiagnosticQueryHandler
     {
-        public async Task<List<string>> GetEnvironmentsAsync()
+        public Task<List<string>> GetEnvironmentsAsync()
         {
-            var logsEnvironments = await RavenHelper.ExecuteAndReturnAsync(session =>
+            return RavenHelper.ExecuteAndReturnAsync(async session =>
             {
-                return session.Query<NodeLogItem>()
+                var logsEnv = await session.Query<NodeLogItem>()
                     .Select(x => x.Environment)
                     .Distinct()
                     .ToListAsync();
-            }).ConfigureAwait(false);
-            var traceEnvironments = await RavenHelper.ExecuteAndReturnAsync(session =>
-            {
-                return session.Query<NodeTraceItem>()
+                var tracesEnv = await session.Query<NodeTraceItem>()
                     .Select(x => x.Environment)
                     .Distinct()
                     .ToListAsync();
-            }).ConfigureAwait(false);
-            var statusEnvironment = await RavenHelper.ExecuteAndReturnAsync(session =>
-            {
-                return session.Query<NodeStatusItem>()
+                var statusEnv = await session.Query<NodeStatusItem>()
                     .Select(x => x.Environment)
                     .Distinct()
                     .ToListAsync();
-            }).ConfigureAwait(false);
-
-            return logsEnvironments.Concat(traceEnvironments).Concat(statusEnvironment).Distinct().RemoveNulls().ToList();
+                return logsEnv.Concat(tracesEnv).Concat(statusEnv).Distinct().RemoveNulls().ToList();
+            });
         }
 
-        //Logs
+        // Logs
         public async Task<LogSummary> GetLogsApplicationsLevelsByEnvironmentAsync(string environment, DateTime fromDate, DateTime toDate)
         {
             var value = await RavenHelper.ExecuteAndReturnAsync(session =>
@@ -110,9 +103,9 @@ namespace TWCore.Diagnostics.Api.MessageHandlers.RavenDb
             };
             return summary;
         }
-        public async Task<PagedList<NodeLogItem>> GetLogsByApplicationLevelsEnvironmentAsync(string environment, string application, LogLevel level, DateTime fromDate, DateTime toDate, int page, int pageSize = 50)
+        public Task<PagedList<NodeLogItem>> GetLogsByApplicationLevelsEnvironmentAsync(string environment, string application, LogLevel level, DateTime fromDate, DateTime toDate, int page, int pageSize = 50)
         {
-            return await RavenHelper.ExecuteAndReturnAsync(async session =>
+            return RavenHelper.ExecuteAndReturnAsync(async session =>
             {
                 var documentQuery = session.Advanced.AsyncDocumentQuery<NodeLogItem>();
                 var query = documentQuery
@@ -144,11 +137,10 @@ namespace TWCore.Diagnostics.Api.MessageHandlers.RavenDb
                     TotalResults = stats.TotalResults,
                     Data = data
                 };
-            }).ConfigureAwait(false);
+            });
         }
 
-
-        //Traces
+        // Traces
         public async Task<PagedList<TraceResult>> GetTracesByEnvironmentAsync(string environment, DateTime fromDate, DateTime toDate, int page, int pageSize = 50)
         {
             var value = await RavenHelper.ExecuteAndReturnAsync(session =>
@@ -189,12 +181,6 @@ namespace TWCore.Diagnostics.Api.MessageHandlers.RavenDb
                 Data = val
             };
         }
-        private class TraceTempResult
-        {
-            public string Group { get; set; }
-            public DateTime Timestamp { get; set; }
-            public string Tags { get; set; }
-        }
         public async Task<List<NodeTraceItem>> GetTracesByGroupIdAsync(string environment, string groupName)
         {
             var value = await RavenHelper.ExecuteAndReturnAsync(session =>
@@ -212,9 +198,34 @@ namespace TWCore.Diagnostics.Api.MessageHandlers.RavenDb
 
             return value;
         }
+        public Task<SerializedObject> GetTraceObjectAsync(string id)
+        {
+            return RavenHelper.ExecuteAndReturnAsync(async session =>
+            {
+                var attachment = await session.Advanced.Attachments.GetAsync(id, "Trace").ConfigureAwait(false);
+                if (attachment == null) return null;
+                var traceObject = attachment.Stream.DeserializeFromNBinary<object>();
+                return (SerializedObject)traceObject;
+            });
+        }
+        private class TraceTempResult
+        {
+            public string Group { get; set; }
+            public DateTime Timestamp { get; set; }
+            public string Tags { get; set; }
+        }
 
+        // Search
+        public async Task<SearchResults> Search(string environment, string searchTerm, DateTime fromDate, DateTime toDate)
+        {
+            return null;
+        }
 
-
+        
+        
+        
+        
+        
         //Others
         public async Task<PagedList<NodeLogItem>> GetLogsByGroupAsync(string environment, string group, string application, DateTime fromDate, DateTime toDate, int page, int pageSize = 50)
         {
@@ -384,17 +395,6 @@ namespace TWCore.Diagnostics.Api.MessageHandlers.RavenDb
             }).ConfigureAwait(false);
         }
 
-        public async Task<SerializedObject> GetTraceObjectAsync(string id)
-        {
-            return await RavenHelper.ExecuteAndReturnAsync(async session =>
-            {
-                var attachment = await session.Advanced.Attachments.GetAsync(id, "Trace").ConfigureAwait(false);
-                if (attachment == null) return null;
-                var traceObject = attachment.Stream.DeserializeFromNBinary<object>();
-                return (SerializedObject)traceObject;
-
-            }).ConfigureAwait(false);
-        }
 
 
         public async Task<PagedList<NodeStatusItem>> GetStatusesAsync(string environment, string machine, string application, DateTime fromDate, DateTime toDate, int page, int pageSize = 50)

a49fe1400efe689e1bf3db7ab4505198f4380d7c
daniel.redondo <daniel.redondo@agsw.es>
2018-07-11 11:59:33 +0200

    Version change.

--- tools/TWCore.Diagnostics.Api/dockerBuild.sh
+++ tools/TWCore.Diagnostics.Api/dockerBuild.sh
@@ -10,9 +10,9 @@ dotnet build -c Release -r linux-x64
 dotnet publish -c Release -r linux-x64 -v q -o ./app/
 
 echo "Building docker image"
-docker build -t twcore_diagnostics:2.1.149 .
-docker tag twcore_diagnostics:2.1.149 tonyredondo/twcore_diagnostics:2.1.149
-docker push tonyredondo/twcore_diagnostics:2.1.149
+docker build -t twcore_diagnostics:2.1.150 .
+docker tag twcore_diagnostics:2.1.150 tonyredondo/twcore_diagnostics:2.1.150
+docker push tonyredondo/twcore_diagnostics:2.1.150
 
 echo "Remove build artifacts"
 rm -r app

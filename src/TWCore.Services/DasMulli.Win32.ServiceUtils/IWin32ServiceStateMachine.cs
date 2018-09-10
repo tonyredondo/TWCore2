47ca887f43fe70d22531b8bca32899467267c831
daniel.redondo <daniel.redondo@agsw.es>
2018-09-10 17:21:59 +0200

    xml docs.

--- src/TWCore.Services/DasMulli.Win32.ServiceUtils/IWin32ServiceStateMachine.cs
+++ src/TWCore.Services/DasMulli.Win32.ServiceUtils/IWin32ServiceStateMachine.cs
@@ -3,9 +3,22 @@
 // ReSharper disable UnusedParameter.Global
 namespace DasMulli.Win32.ServiceUtils
 {
+    /// <summary>
+    /// Win32 Service StateMachine interface
+    /// </summary>
     public interface IWin32ServiceStateMachine
     {
+        /// <summary>
+        /// On Start command
+        /// </summary>
+        /// <param name="startupArguments">Start arguments</param>
+        /// <param name="statusReportCallback">Status report callback</param>
         void OnStart(string[] startupArguments, ServiceStatusReportCallback statusReportCallback);
+        /// <summary>
+        /// On Command
+        /// </summary>
+        /// <param name="command">Service control command</param>
+        /// <param name="commandSpecificEventType">Command specific event type</param>
         void OnCommand(ServiceControlCommand command, uint commandSpecificEventType);
     }
 }
--- src/TWCore.Services/DasMulli.Win32.ServiceUtils/ServiceStatusReportCallback.cs
+++ src/TWCore.Services/DasMulli.Win32.ServiceUtils/ServiceStatusReportCallback.cs
@@ -2,5 +2,12 @@
 
 namespace DasMulli.Win32.ServiceUtils
 {
+    /// <summary>
+    /// Service status report callback
+    /// </summary>
+    /// <param name="state">Service state</param>
+    /// <param name="acceptedControlCommands">Accepted control commands</param>
+    /// <param name="win32ExitCode">Win32 exit code</param>
+    /// <param name="waitHint">Wait hint</param>
     public delegate void ServiceStatusReportCallback(ServiceState state, ServiceAcceptedControlCommandsFlags acceptedControlCommands, int win32ExitCode, uint waitHint);
 }

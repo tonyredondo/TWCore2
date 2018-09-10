b5c81098df0238a4a8cf185cc242004d0f1d420d
daniel.redondo <daniel.redondo@agsw.es>
2018-09-10 18:15:02 +0200

    xml docs

--- src/TWCore.Services/DasMulli.Win32.ServiceUtils/HashCode.cs
+++ src/TWCore.Services/DasMulli.Win32.ServiceUtils/HashCode.cs
@@ -11,27 +11,57 @@ namespace DasMulli.Win32.ServiceUtils
     public struct HashCode
     {
         private readonly int _value;
+
+        #region .ctor
         private HashCode(int value)
         {
             _value = value;
         }
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Converts a hashcode struct to the int value
+        /// </summary>
+        /// <param name="hashCode">Hashcode instance</param>
         public static implicit operator int(HashCode hashCode)
         {
             return hashCode._value;
         }
+        /// <summary>
+        /// Gets the HashCode of an item
+        /// </summary>
+        /// <typeparam name="T">Type of the item</typeparam>
+        /// <param name="item">Item instance</param>
+        /// <returns>Hashcode value</returns>
         public static HashCode Of<T>(T item)
         {
             return new HashCode(GetHashCode(item));
         }
+        /// <summary>
+        /// Combine hashcode with the hashcode of other item
+        /// </summary>
+        /// <typeparam name="T">Type of the item</typeparam>
+        /// <param name="item">Item instance</param>
+        /// <returns>Hashcode value resulted of the combination of both hashcodes</returns>
         public HashCode And<T>(T item)
         {
             return new HashCode(CombineHashCodes(_value, GetHashCode(item)));
         }
+        /// <summary>
+        /// Combine hashcode with the hashcode of all items on the ienumerable
+        /// </summary>
+        /// <typeparam name="T">Type of the item</typeparam>
+        /// <param name="items">Items ienumerable instance</param>
+        /// <returns>Hashcode value resulted of the combination of both hashcodes</returns>
         public HashCode AndEach<T>(IEnumerable<T> items)
         {
             var hashCode = items.Select(GetHashCode).Aggregate(CombineHashCodes);
             return new HashCode(CombineHashCodes(_value, hashCode));
         }
+        #endregion
+
+        #region Private Methods
         private static int CombineHashCodes(int h1, int h2)
         {
             unchecked
@@ -42,7 +72,8 @@ namespace DasMulli.Win32.ServiceUtils
         }
         private static int GetHashCode<T>(T item)
         {
-            return item == null ? 0 : item.GetHashCode();
+            return item?.GetHashCode() ?? 0;
         }
+        #endregion
     }
 }
--- src/TWCore.Services/DasMulli.Win32.ServiceUtils/IWin32Service.cs
+++ src/TWCore.Services/DasMulli.Win32.ServiceUtils/IWin32Service.cs
@@ -3,14 +3,46 @@
 // ReSharper disable UnusedParameter.Global
 namespace DasMulli.Win32.ServiceUtils
 {
+    /// <summary>
+    /// Service stopped callback delegate
+    /// </summary>
     public delegate void ServiceStoppedCallback();
+    
+    /// <summary>
+    /// Win32 Service interface
+    /// </summary>
     public interface IWin32Service
     {
+        #region Properties
+        /// <summary>
+        /// Gets if the service can pause and continue
+        /// </summary>
         bool CanPauseAndContinue { get; }
+        /// <summary>
+        /// Service name
+        /// </summary>
         string ServiceName { get; }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Starts the service
+        /// </summary>
+        /// <param name="startupArguments">Startup arguments</param>
+        /// <param name="serviceStoppedCallback">Service stopped callback delegate instance</param>
         void Start(string[] startupArguments, ServiceStoppedCallback serviceStoppedCallback);
+        /// <summary>
+        /// Stops the service
+        /// </summary>
         void Stop();
+        /// <summary>
+        /// Pause the service
+        /// </summary>
         void OnPause();
+        /// <summary>
+        /// Unpause the service
+        /// </summary>
         void OnContinue();
+        #endregion
     }
 }

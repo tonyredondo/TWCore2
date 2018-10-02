aaadb83d3266bf9664cb226398d86b32004d5f27
daniel.redondo <daniel.redondo@agsw.es>
2018-10-02 19:11:07 +0200

    change.

---
+++ src/TWCore.Messaging.Kafka/KafkaQueueClient.cs
@@ -0,0 +1,85 @@
+﻿/*
+Copyright 2015-2018 Daniel Adrian Redondo Suarez
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TWCore.Messaging.Client;
+
+// ReSharper disable NotAccessedField.Local
+// ReSharper disable InconsistentNaming
+
+namespace TWCore.Messaging.Kafka
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Kafka Queue Client
+    /// </summary>
+    public class KafkaQueueClient : MQueueClientBase
+    {
+        #region Init and Dispose Methods
+        /// <inheritdoc />
+        /// <summary>
+        /// On client initialization
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected override void OnInit()
+        {
+            throw new NotImplementedException();
+        }
+        /// <inheritdoc />
+        /// <summary>
+        /// On Dispose
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected override void OnDispose()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Send Method
+        /// <inheritdoc />
+        /// <summary>
+        /// On Send message data
+        /// </summary>
+        /// <param name="message">Request message instance</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected override Task<bool> OnSendAsync(RequestMessage message)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        #region Receive Method
+        /// <inheritdoc />
+        /// <summary>
+        /// On Receive message data
+        /// </summary>
+        /// <param name="correlationId">Correlation Id</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>Response message instance</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        protected override Task<ResponseMessage> OnReceiveAsync(Guid correlationId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}

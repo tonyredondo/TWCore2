007d879779cf5632dc832eee065b5f3208f87a34
daniel.redondo <daniel.redondo@agsw.es>
2018-10-16 22:13:20 +0200

    changes.

--- src/TWCore.Serialization.NSerializer/GenericObject.cs
+++ src/TWCore.Serialization.NSerializer/GenericObject.cs
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -27,273 +27,281 @@ using TWCore.Collections;
 
 namespace TWCore.Serialization.NSerializer
 {
-    /// <summary>
-    /// Generic object
-    /// </summary>
-    [DataContract]
-    public class GenericObject
-    {
-        [DebuggerBrowsable(DebuggerBrowsableState.Never)] private bool _isArray;
-        [DebuggerBrowsable(DebuggerBrowsableState.Never)] private bool _isList;
-        [DebuggerBrowsable(DebuggerBrowsableState.Never)] private bool _isDictionary;
+	/// <summary>
+	/// Generic object
+	/// </summary>
+	[DataContract]
+	public class GenericObject
+	{
+		[DebuggerBrowsable(DebuggerBrowsableState.Never)] private bool _isArray;
+		[DebuggerBrowsable(DebuggerBrowsableState.Never)] private bool _isList;
+		[DebuggerBrowsable(DebuggerBrowsableState.Never)] private bool _isDictionary;
 
-        #region Properties
-        /// <summary>
-        /// Type name
-        /// </summary>
-        [XmlAttribute, DataMember, Newtonsoft.Json.JsonProperty(
-        	NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore,
+		#region Properties
+		/// <summary>
+		/// Type name
+		/// </summary>
+		[XmlAttribute, DataMember, Newtonsoft.Json.JsonProperty(
+			NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore,
 			TypeNameHandling = Newtonsoft.Json.TypeNameHandling.None,
 			ItemTypeNameHandling = Newtonsoft.Json.TypeNameHandling.None)]
-        public string Type { get; set; }
-        /// <summary>
-        /// Is Array Object
-        /// </summary>
-        [XmlIgnore]
-        public bool IsArray
-        {
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => Array != null || _isArray;
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            set => _isArray = value;
-        }
-        /// <summary>
-        /// Is List Object
-        /// </summary>
-        [XmlIgnore]
-        public bool IsList
-        {
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => List != null || _isList;
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            set => _isList = value;
-        }
-        /// <summary>
-        /// Is Dictionary Object
-        /// </summary>
-        [XmlIgnore]
-        public bool IsDictionary
-        {
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get => Dictionary != null || _isDictionary;
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            set => _isDictionary = value;
-        }
-        /// <summary>
-        /// Properties
-        /// </summary>
-        [DataMember, Newtonsoft.Json.JsonProperty(
-            NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore, 
-            TypeNameHandling = Newtonsoft.Json.TypeNameHandling.None, 
-            ItemTypeNameHandling = Newtonsoft.Json.TypeNameHandling.None)]
-        public Dictionary<string, GenericObject> Properties { get; set; }
-        /// <summary>
-        /// Dictionary Data
-        /// </summary>
-        [DataMember, Newtonsoft.Json.JsonProperty(
-            NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore,
-            TypeNameHandling = Newtonsoft.Json.TypeNameHandling.None,
-            ItemTypeNameHandling = Newtonsoft.Json.TypeNameHandling.None)]
-        public Dictionary<object, GenericObject> Dictionary { get; set; }
-        /// <summary>
-        /// List Data
-        /// </summary>
-        [DataMember, Newtonsoft.Json.JsonProperty(
-            NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore,
-            TypeNameHandling = Newtonsoft.Json.TypeNameHandling.None,
-            ItemTypeNameHandling = Newtonsoft.Json.TypeNameHandling.None)]
-        public List<GenericObject> List { get; set; }
-        /// <summary>
-        /// Array Data
-        /// </summary>
-        [DataMember, Newtonsoft.Json.JsonProperty(
-            NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore,
-            TypeNameHandling = Newtonsoft.Json.TypeNameHandling.None,
-            ItemTypeNameHandling = Newtonsoft.Json.TypeNameHandling.None)]
-        public GenericObject[] Array { get; set; }
+		public string Type { get; set; }
 		/// <summary>
-		/// Value Data
+		/// Is Array Object
+		/// </summary>
+		[XmlIgnore]
+		public bool IsArray
+		{
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			get => Array != null || _isArray;
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			set => _isArray = value;
+		}
+		/// <summary>
+		/// Is List Object
+		/// </summary>
+		[XmlIgnore]
+		public bool IsList
+		{
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			get => List != null || _isList;
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			set => _isList = value;
+		}
+		/// <summary>
+		/// Is Dictionary Object
+		/// </summary>
+		[XmlIgnore]
+		public bool IsDictionary
+		{
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			get => Dictionary != null || _isDictionary;
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			set => _isDictionary = value;
+		}
+		/// <summary>
+		/// Properties
+		/// </summary>
+		[DataMember, Newtonsoft.Json.JsonProperty(
+			NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore, 
+			TypeNameHandling = Newtonsoft.Json.TypeNameHandling.None, 
+			ItemTypeNameHandling = Newtonsoft.Json.TypeNameHandling.None)]
+		public Dictionary<string, GenericObject> Properties { get; set; }
+		/// <summary>
+		/// Dictionary Data
 		/// </summary>
 		[DataMember, Newtonsoft.Json.JsonProperty(
 			NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore,
 			TypeNameHandling = Newtonsoft.Json.TypeNameHandling.None,
 			ItemTypeNameHandling = Newtonsoft.Json.TypeNameHandling.None)]
+		public Dictionary<object, GenericObject> Dictionary { get; set; }
+		/// <summary>
+		/// List Data
+		/// </summary>
+		[DataMember, Newtonsoft.Json.JsonProperty(
+			NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore,
+			TypeNameHandling = Newtonsoft.Json.TypeNameHandling.None,
+			ItemTypeNameHandling = Newtonsoft.Json.TypeNameHandling.None)]
+		public List<GenericObject> List { get; set; }
+		/// <summary>
+		/// Array Data
+		/// </summary>
+		[DataMember, Newtonsoft.Json.JsonProperty(
+			NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore,
+			TypeNameHandling = Newtonsoft.Json.TypeNameHandling.None,
+			ItemTypeNameHandling = Newtonsoft.Json.TypeNameHandling.None)]
+		public GenericObject[] Array { get; set; }
+		/// <summary>
+		/// Value Data
+		/// </summary>
+		[DataMember, Newtonsoft.Json.JsonProperty(
+			NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
 		public object Value { get; set; }
-        #endregion
+		#endregion
 
-        #region .ctor
-        /// <summary>
-        /// Generic object
-        /// </summary>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public GenericObject() { }
-        /// <summary>
-        /// Generic object
-        /// </summary>
-        /// <param name="metaData">Generic deserializer metadata</param>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public GenericObject(GenericDeserializerMetaDataOfType metaData)
-        {
-            if (metaData != null)
-            {
-                Type = metaData.Type;
-                IsArray = metaData.IsArray;
-                IsList = metaData.IsList;
-                IsDictionary = metaData.IsDictionary;
+		#region .ctor
+		/// <summary>
+		/// Generic object
+		/// </summary>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public GenericObject() { }
+		/// <summary>
+		/// Generic object
+		/// </summary>
+		/// <param name="metaData">Generic deserializer metadata</param>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		public GenericObject(GenericDeserializerMetaDataOfType metaData)
+		{
+			if (metaData != null)
+			{
+				Type = metaData.Type;
+				IsArray = metaData.IsArray;
+				IsList = metaData.IsList;
+				IsDictionary = metaData.IsDictionary;
 
-                if (metaData.Properties?.Any() == true)
-                {
-                    Properties = new Dictionary<string, GenericObject>();
-                    foreach (var property in metaData.Properties)
-                        Properties[property] = null;
-                }
-                if (IsList)
-                {
-                    List = new List<GenericObject>();
-                }
-                if (IsDictionary)
-                {
-                    Dictionary = new Dictionary<object, GenericObject>();
-                }
-            }
-        }
-        #endregion
+				if (metaData.Properties?.Any() == true)
+				{
+					Properties = new Dictionary<string, GenericObject>();
+					foreach (var property in metaData.Properties)
+						Properties[property] = null;
+				}
+				if (IsList)
+				{
+					List = new List<GenericObject>();
+				}
+				if (IsDictionary)
+				{
+					Dictionary = new Dictionary<object, GenericObject>();
+				}
+			}
+		}
+		#endregion
 
-        #region Public Methods
-        /// <summary>
-        /// Get the object property value
-        /// </summary>
-        /// <param name="key">Object property name</param>
-        /// <returns>Property value</returns>
-        public GenericObject this[string key]
-        {
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get
-            {
-                if (IsDictionary && Dictionary.TryGetValue(key, out var dictioValue))
-                    return dictioValue;
-                if (Properties.TryGetValue(key, out var value))
-                    return value;
-                if (!IsDictionary)
-                    throw new Exception("Property is not found in the object");
-                throw new KeyNotFoundException();
-            }
-        }
-        /// <summary>
-        /// Get the object dictionary value
-        /// </summary>
-        /// <param name="key">Object dictionary name</param>
-        /// <returns>Property value</returns>
-        public GenericObject this[object key]
-        {
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get
-            {
-                if (IsDictionary && Dictionary.TryGetValue(key, out var dictioValue))
-                    return dictioValue;
-                if (!IsDictionary)
-                    throw new Exception("The object is not a Dictionary");
-                throw new KeyNotFoundException();
-            }
-        }
-        /// <summary>
-        /// Get the index value of an array or list
-        /// </summary>
-        /// <param name="index">Index number</param>
-        /// <returns>Object value</returns>
-        public GenericObject this[int index]
-        {
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            get
-            {
-                if (IsArray)
-                {
-                    if (Array != null)
-                        return Array[index];
-                    else
-                        throw new Exception("The ArrayData has not been initialized");
-                }
-                if (IsList)
-                    return List[index];
-                throw new Exception("The object is not an Array/List");
-            }
-        }
-        #endregion
+		#region Public Methods
+		/// <summary>
+		/// Get the object property value
+		/// </summary>
+		/// <param name="key">Object property name</param>
+		/// <returns>Property value</returns>
+		public GenericObject this[string key]
+		{
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			get
+			{
+				if (IsDictionary && Dictionary.TryGetValue(key, out var dictioValue))
+					return dictioValue;
+				if (Properties.TryGetValue(key, out var value))
+					return value;
+				if (!IsDictionary)
+					throw new Exception("Property is not found in the object");
+				throw new KeyNotFoundException();
+			}
+		}
+		/// <summary>
+		/// Get the object dictionary value
+		/// </summary>
+		/// <param name="key">Object dictionary name</param>
+		/// <returns>Property value</returns>
+		public GenericObject this[object key]
+		{
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			get
+			{
+				if (IsDictionary && Dictionary.TryGetValue(key, out var dictioValue))
+					return dictioValue;
+				if (!IsDictionary)
+					throw new Exception("The object is not a Dictionary");
+				throw new KeyNotFoundException();
+			}
+		}
+		/// <summary>
+		/// Get the index value of an array or list
+		/// </summary>
+		/// <param name="index">Index number</param>
+		/// <returns>Object value</returns>
+		public GenericObject this[int index]
+		{
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			get
+			{
+				if (IsArray)
+				{
+					if (Array != null)
+						return Array[index];
+					else
+						throw new Exception("The ArrayData has not been initialized");
+				}
+				if (IsList)
+					return List[index];
+				throw new Exception("The object is not an Array/List");
+			}
+		}
+		#endregion
 
-        #region Internal Methods
-        /// <summary>
-        /// Set property value
-        /// </summary>
-        /// <param name="key">Object property key</param>
-        /// <param name="value">Object property value</param>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal void SetProperty(string key, object value)
-        {
+		#region Internal Methods
+		/// <summary>
+		/// Set property value
+		/// </summary>
+		/// <param name="key">Object property key</param>
+		/// <param name="value">Object property value</param>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		internal void SetProperty(string key, object value)
+		{
 			if (value is GenericObject gObject)
 				Properties[key] = gObject;
+			else if (value is null)
+				Properties[key] = null;
 			else
 				Properties[key] = new GenericObject { Value = value };
-        }
-        /// <summary>
-        /// Init array value
-        /// </summary>
-        /// <param name="capacity">Array capacity</param>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal void InitArray(int capacity)
-        {
-            Array = new GenericObject[capacity];
-        }
-        /// <summary>
-        /// Set an array value
-        /// </summary>
-        /// <param name="index">Index of the array</param>
-        /// <param name="value">Object value</param>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal void SetArrayValue(int index, object value)
-        {
+		}
+		/// <summary>
+		/// Init array value
+		/// </summary>
+		/// <param name="capacity">Array capacity</param>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		internal void InitArray(int capacity)
+		{
+			Array = new GenericObject[capacity];
+		}
+		/// <summary>
+		/// Set an array value
+		/// </summary>
+		/// <param name="index">Index of the array</param>
+		/// <param name="value">Object value</param>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		internal void SetArrayValue(int index, object value)
+		{
 			if (value is GenericObject gObject)
 				Array[index] = gObject;
+			else if (value is null)
+				Array[index] = null;
 			else
 				Array[index] = new GenericObject { Value = value };
 		}
-        /// <summary>
-        /// Set a list value
-        /// </summary>
-        /// <param name="index">Index of the list</param>
-        /// <param name="value">Object value</param>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal void SetListValue(int index, object value)
-        {
+		/// <summary>
+		/// Set a list value
+		/// </summary>
+		/// <param name="index">Index of the list</param>
+		/// <param name="value">Object value</param>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		internal void SetListValue(int index, object value)
+		{
 			if (value is GenericObject gObject)
 				List[index] = gObject;
+			else if (value is null)
+				List[index] = null;
 			else
 				List[index] = new GenericObject { Value = value };
-        }
-        /// <summary>
-        /// Add a list value
-        /// </summary>
-        /// <param name="value">Object value</param>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal void AddListValue(object value)
-        {
+		}
+		/// <summary>
+		/// Add a list value
+		/// </summary>
+		/// <param name="value">Object value</param>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		internal void AddListValue(object value)
+		{
 			if (value is GenericObject gObject)
 				List.Add(gObject);
+			else if (value is null)
+				List.Add(null);
 			else
 				List.Add(new GenericObject { Value = value });
-        }
-        /// <summary>
-        /// Set a dictionary key/value
-        /// </summary>
-        /// <param name="key">Dictionary key</param>
-        /// <param name="value">Dictionary value</param>
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        internal void SetDictionaryValue(object key, object value)
-        {
+		}
+		/// <summary>
+		/// Set a dictionary key/value
+		/// </summary>
+		/// <param name="key">Dictionary key</param>
+		/// <param name="value">Dictionary value</param>
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		internal void SetDictionaryValue(object key, object value)
+		{
 			if (value is GenericObject gObject)
 				Dictionary[key] = gObject;
+			else if (value is null)
+				Dictionary[key] = null;
 			else
 				Dictionary[key] = new GenericObject { Value = value };
-        }
+		}
 		#endregion
 	}
 }

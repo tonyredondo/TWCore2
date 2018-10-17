90216f420969b59370604812f305cfb99d05e074
daniel.redondo <daniel.redondo@agsw.es>
2018-10-17 13:24:01 +0200

    new injector engine start

---
+++ src/TWCore/Injector/NewInjectorEngine.cs
@@ -0,0 +1,51 @@
+﻿/*
+Copyright 2015-2018 Daniel Adrian Redondo Suarez
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using TWCore.Collections;
+using TWCore.Diagnostics.Log;
+using TWCore.Text;
+// ReSharper disable EventNeverSubscribedTo.Global
+// ReSharper disable UnusedMember.Global
+// ReSharper disable MemberCanBePrivate.Global
+
+namespace TWCore.Injector
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// Inject instances engine
+    /// </summary>
+    public class NewInjectorEngine : IDisposable
+    {
+
+
+        /// <inheritdoc />
+        /// <summary>
+        /// Dispose resources
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public void Dispose()
+        {
+        }
+
+    }
+}

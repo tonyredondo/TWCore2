c8f46eead451828e2c693486e562bb555849ae18
Daniel Redondo <tonyredondo@gmail.com>
2019-01-06 23:17:58 +0100

    Bench init commit

---
+++ test/TWCore.Tests.Benchmark/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TWCore.Tests.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
